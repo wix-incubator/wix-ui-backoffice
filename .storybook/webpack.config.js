ba8bcb8d53945076f09626f88304a665a0a8fc08
Lior Belinsky <liorbe@wix.com>
2017-12-04 18:53:27 +0200

    haste migration of backoffice, core, santa and test-utils

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,5 +1,5 @@
 const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
-const wixStorybookConfig = require('yoshi/config/webpack.config.storybook');
+const wixStorybookConfig = require('haste-preset-yoshi/config/webpack.config.storybook');
 
 module.exports = (config, env) => {
   return wixStorybookConfig(genDefaultConfig(config, env));
