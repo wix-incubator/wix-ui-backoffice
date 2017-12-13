66c8af8203d9c694d23284ce58fe5ae98e2d40d3
Lior Belinsky <liorbe@wix.com>
2017-12-13 23:33:38 +0200

    Hbox (#47)
    * add Hbox component core + backoffice
    
    * small change
    
    * test core Hbox
    
    * add hieht to style
    
    * add height prop to backoffice HBox
    
    * try to rename Hbox
    
    * rename Hbox to HBox
    
    * try to rename Hbox
    
    * rename HBox core
    
    * change api
    
    * add width prop
    
    * pass props from backoffice component
    
    * fix lint
    
    * Update index.js

---
+++ src/components/HBox/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {oneOf, string} from 'prop-types';
+import CoreHBox from 'wix-ui-core/HBox';
+import {ThemedComponent} from 'wix-ui-theme';
+import {theme} from './theme';
+
+const HBox = ({spacing, verticalAlignment, height, width, ...coreProps}) => (
+  <ThemedComponent theme={theme} spacing={spacing} height={height} width={width} verticalAlignment={verticalAlignment}>
+    <CoreHBox {...coreProps}/>
+  </ThemedComponent>
+);
+
+HBox.propTypes = {
+  ...CoreHBox.propTypes,
+
+  /** Spacing between the elements */
+  spacing: oneOf(['small', 'medium', 'large']),
+
+  /** Similar to flexbox vertical-align */
+  verticalAlignment: oneOf(['top', 'center', 'bottom']),
+
+  /** Height of the HBox container */
+  height: string,
+
+  /** Width of the HBox container */
+  width: string
+};
+
+export default HBox;
---
+++ src/components/HBox/theme.js
@@ -0,0 +1,6 @@
+export const theme = ({spacing, verticalAlignment, height, width}) => ({
+  spacing,
+  verticalAlignment,
+  height,
+  width
+});
