9ccbf75e13880c057af043f9d7531ec5d6d19696
Lior Belinsky <liorbe@wix.com>
2017-12-19 11:19:07 +0200

    update package git url for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -11,6 +11,6 @@ configure(loadStories, module);
 setOptions({
   showDownPanel: false,
   name: 'wix-ui-backoffice',
-  url: 'https://github.com/wix/wix-ui/packages/wix-ui-backoffice',
+  url: 'https://github.com/wix/wix-ui-backoffice',
   sidebarAnimations: true
 });
