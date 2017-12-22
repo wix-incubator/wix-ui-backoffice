88062df78e6570ea04adda195d0432c1f64646f3
Alan Dardic <alandardic9@gmail.com>
2017-12-22 09:10:06 +0200

    fix(toggleSwitch): removed unnecessary property

--- src/components/ToggleSwitch/index.js
+++ src/components/ToggleSwitch/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {any, oneOf} from 'prop-types';
+import {oneOf} from 'prop-types';
 import CoreToggleSwitch from 'wix-ui-core/ToggleSwitch';
 import {ThemedComponent} from 'wix-ui-theme';
 import {theme} from './theme';
@@ -20,10 +20,7 @@ ToggleSwitch.propTypes = {
   size: oneOf(['x-small', 'small', 'large']),
 
   /** Color for disabled toggle switch */
-  skin: oneOf(['standard', 'error', 'success']),
-
-  /** Content of the badge */
-  children: any
+  skin: oneOf(['standard', 'error', 'success'])
 };
 
 ToggleSwitch.defaultProps = {
