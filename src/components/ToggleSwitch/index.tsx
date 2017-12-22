2297ce365d518491b5737a3c1fb90bf741fbecd5
Iurii Komarov <iuriikomarov@icloud.com>
2017-12-22 16:06:57 +0200

    Migrate ToggleSwitch

---
+++ src/components/ToggleSwitch/index.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import CoreToggleSwitch from 'wix-ui-core/ToggleSwitch';
+import {ThemedComponent} from 'wix-ui-theme';
+import {theme} from './theme';
+import UIText from '../UIText';
+
+interface Props { // TODO: extend CoreToggleSwitchProps,
+  /** size of the toggle switch */
+  size: 'x-small' | 'small' | 'large';
+
+  /** Color for disabled toggle switch */
+  skin: 'standard' | 'error' | 'success';
+};
+
+
+export default class ToggleSwitch extends React.PureComponent<Props>  {
+  static defaultProps = {
+    size: 'large',
+    skin: 'standard'
+  };
+
+  render() {
+    const {size, skin, children, ...coreProps} = this.props;
+
+    return (
+      <ThemedComponent {...{theme, size, skin}}>
+        <CoreToggleSwitch {...coreProps}>
+          <UIText appearance="T1.1" dataClass="toggle-switch-content">{children}</UIText>
+        </CoreToggleSwitch>
+      </ThemedComponent>
+    );
+  }
+}
+
