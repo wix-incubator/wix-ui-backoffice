6e73408bb56d8f2063972faf47e25e910d1e2049
Shlomi Toussia-Cohen <shlomitc@wix.com>
2018-01-21 18:57:27 +0200

    CounterBadge - allow passing any element as children

--- src/components/CounterBadge/index.tsx
+++ src/components/CounterBadge/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import {Badge as CoreBadge, BadgeProps as CoreBadgeProps} from 'wix-ui-core/Badge';
+import {oneOf, any} from 'prop-types';
 import {ThemedComponent} from 'wix-ui-theme';
+import {Badge as CoreBadge, BadgeProps as CoreBadgeProps} from 'wix-ui-core/Badge';
 import {theme} from './theme';
 import {UIText} from '../UIText';
-import {oneOf, oneOfType, string, number} from 'prop-types';
 import {Skin} from './constants';
 
 export {Skin};
@@ -12,7 +12,7 @@ interface Props extends CoreBadgeProps {
   skin?: Skin;
 
   /** Content of the badge */
-  children?: string | number;
+  children?: any;
 }
 
 export class CounterBadge extends React.PureComponent<Props> {
@@ -22,7 +22,7 @@ export class CounterBadge extends React.PureComponent<Props> {
     /** Type of the badge */
     skin: oneOf(['default', 'standard', 'urgent', 'success']),
     /** Content of the badge */
-    children: oneOfType([string, number])
+    children: any
   };
 
   static defaultProps: Props = {
@@ -31,15 +31,15 @@ export class CounterBadge extends React.PureComponent<Props> {
 
   render() {
     const {skin, children, ...coreProps} = this.props;
-
-    if (children.toString().length > 2) {
+    const content = (React.Children.map(children, child => child))[0];
+    if ((typeof content === 'string' || typeof content === 'number') && content.toString().length > 2) {
       throw new Error('CounterBadge children max length must be less than 2');
     }
 
     return (
       <ThemedComponent {...{theme, skin}}>
         <CoreBadge {...coreProps}>
-          <UIText appearance="T5" dataClass="badge-content">{children}</UIText>
+          <UIText appearance="T5" dataClass="badge-content">{content}</UIText>
         </CoreBadge>
       </ThemedComponent>
     );
