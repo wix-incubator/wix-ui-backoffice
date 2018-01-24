434cfc55fc5ca3d9a6d424a6f50a4e9732a17b40
Lior Zisman <liorzi@wix.com>
2018-01-24 13:59:50 +0200

    improvements of demo

--- stories/Tooltip/index.tsx
+++ stories/Tooltip/index.tsx
@@ -5,20 +5,25 @@ import {Tooltip} from '../../src/components/Tooltip';
 function createTooltip(direction) {
   return (
     <Tooltip data-hook={`story-tooltip-${direction}`} placement={direction}
-             minWidth={'200px'} textAlign={'center'} padding={'5px'}
+             minWidth={'100px'} textAlign={'center'} padding={'5px'}
              content={<p>This is my tooltip</p>}>
       <span>I need a tooltip</span>
     </Tooltip>
   );
 }
 
-class FullTooltip extends React.Component<any, {shown: boolean}> {
+class FullTooltip extends React.Component<{direction: string}> {
   render() {
-    return <div style={{margin: '5em'}}>{createTooltip('right')}</div>;
+    return <div style={{margin: '5em 15em'}}>{createTooltip(this.props.direction)}</div>;
   }
 }
 
 export const story = () => storiesOf('Components', module)
   .add('Tooltip', () => (
-    <FullTooltip />
+    <div>
+      <FullTooltip direction="right"/>
+      <FullTooltip direction="top"/>
+      <FullTooltip direction="left"/>
+      <FullTooltip direction="bottom"/>
+    </div>
   ));
