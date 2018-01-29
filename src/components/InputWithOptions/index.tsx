2405349422b583058cefaf648a1c7f8ad0c36bf2
Amir Yonatan <amiry@wix.com>
2018-01-29 16:38:38 +0200

    Added input with options changes

--- src/components/InputWithOptions/index.tsx
+++ src/components/InputWithOptions/index.tsx
@@ -3,13 +3,9 @@ import style from './InputWithOptions.st.css';
 import {withStylable} from 'wix-ui-core';
 
 export interface InputWithOptionsProps {
-  skin?: 'standard' | 'error' | 'success';
-  size?: 'large' | 'small' | 'x-small';
 }
 
 const defaultProps = {
-  skin: 'standard',
-  size: 'large'
 };
 
 export const InputWithOptions = withStylable<CoreInputWithOptionsProps, InputWithOptionsProps>(
