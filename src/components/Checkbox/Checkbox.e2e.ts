9e23602bed2fe9094ffd3c2870cd2a9174fb4732
Kieran Williams <kieranw@wix.com>
2018-01-31 14:26:38 +0200

    Added tests and fixed data-hook

--- src/components/Checkbox/Checkbox.e2e.ts
+++ src/components/Checkbox/Checkbox.e2e.ts
@@ -15,60 +15,20 @@ describe('Checkbox', () => {
     expect(driver.element()).toBeDefined();
   });
 
-  // eyes.it('should be unchecked and not disabled by default', () => {
-  //   const driver = checkboxTestkitFactory({ dataHook });
+  eyes.it('should be unchecked and not disabled by default', () => {
+    const driver = checkboxTestkitFactory({dataHook});
 
-  //   // expect(driver.isChecked()).toBeFalsy();
-  //   expect(driver.isDisabled()).toBeFalsy();
-  // });
+    expect(driver.isChecked()).toBe(false);
+    expect(driver.isDisabled()).toBe(false);
+  });
 
-  // it('should be checked', () => {
-  //   const driver = createDriver(<Checkbox checked />);
-  //   expect(driver.isChecked()).toBeTruthy();
-  // });
+  eyes.it('should become checked', () => {
+    const driver = checkboxTestkitFactory({dataHook});
 
-  // it('should be disabled', () => {
-  //   const driver = createDriver(<Checkbox disabled />);
-  //   expect(driver.isDisabled()).toBeTruthy();
-  // });
+    expect(driver.isChecked()).toBe(false);
 
-  // it('should have an error state', () => {
-  //   const driver = createDriver(<Checkbox hasError />);
-  //   expect(driver.hasError()).toBeTruthy();
-  // });
+    driver.click();
 
-  // it('should have a label', () => {
-  //   const driver = createDriver(<Checkbox disabled > Hey < /Checkbox>);
-  //   expect(driver.getLabel()).toBe('Hey');
-  // });
-
-  // it('should call onChange when clicking the Checkbox', () => {
-  //   const onChange = jest.fn();
-
-  //   const driver = createDriver(<Checkbox onChange={ onChange } />);
-
-  //   driver.click();
-  //   expect(onChange).toBeCalled();
-  // });
-
-  // it('should not call onChange when clicking disabled Checkbox', () => {
-  //   const onChange = jest.fn();
-
-  //   const driver = createDriver(<Checkbox onChange={ onChange } disabled />);
-
-  //   driver.click();
-  //   expect(onChange).not.toBeCalled();
-  // });
-
-  // it('should not run in indeterminate mode when not specified', () => {
-  //   const driver = createDriver(<Checkbox/>);
-
-  //   expect(driver.isIndeterminate()).toBeFalsy();
-  // });
-
-  // it('should run in indeterminate mode when specified', () => {
-  //   const driver = createDriver(<Checkbox indeterminate />);
-
-  //   expect(driver.isIndeterminate()).toBeTruthy();
-  // });
+    expect(driver.isChecked()).toBe(true);
+  });
 });
