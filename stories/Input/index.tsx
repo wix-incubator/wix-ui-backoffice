e802b15b7f66839e7637247abdf7e20acdddcf30
Daniel Strokovsky <danielstr@wix.com>
2018-02-05 11:39:26 +0200

    clean up

--- stories/Input/index.tsx
+++ stories/Input/index.tsx
@@ -4,24 +4,7 @@ import {storiesOf} from '@storybook/react';
 import {Input} from '../../src/components/Input';
 import * as InputSource from '!raw-loader!../../src/components/Input/index.tsx';
 
-// class ControlledToggle extends React.Component<any, any> {
-//   render() {
-//     return <Input {...this.props} />;
-//   }
-// }
-
 export const story = () => storiesOf('Components', module)
   .add('Input', () => (
       <Input data-hook="storybook-input"/>
   ));
-
-// export const story = () => createStory({
-//   category: 'Components',
-//   name: 'Input',
-//   storyName: 'Input',
-//   component: Input,
-//   componentProps: () => ({
-//     'data-hook': 'storybook-input'
-//   }),
-//   source: InputSource
-// });
