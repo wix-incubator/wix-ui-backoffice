8e2d23b74f9161fd50ece992f39cb77728dfe698
Amir Yonatan <amir.yonatan1@gmail.com>
2018-02-08 19:11:09 +0200

    remove index file (#34)
    * remove index file
    
    * remove index file
    
    * Added test
    
    * Fixed transform
    
    * ignore jss test

--- src/components/Divider/Divider.spec.tsx
+++ src/components/Divider/Divider.spec.tsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import {dividerDriverFactory} from './Divider.driver';
 import {Divider} from './';
-import {mount} from 'enzyme';
 import {createDriverFactory} from 'wix-ui-test-utils/driver-factory';
 
 describe('Divider', () => {
   const createDriver = createDriverFactory(dividerDriverFactory);
 
   it('should render divider', () => {
-    let wrapper = mount(
-      <Divider/>,
-      {attachTo: document.createElement('div')}
-    );
-    expect(wrapper.find('input').length).toBe(1);
+    const wrapper = createDriver(<Divider/>);
+    expect(wrapper.exists()).toBeTruthy();
   });
 });
