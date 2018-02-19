a1338d3f87fe30b67d89810cdf75cfa85598d108
Lior Belinsky <liorbe@LiorBe-MBP15-2.local>
2018-02-19 18:53:01 +0200

    add e2e test

---
+++ src/components/Heading/Heading.e2e.ts
@@ -0,0 +1,17 @@
+import * as eyes from 'eyes.it';
+import {browser} from 'protractor';
+import {getStoryUrl, waitForVisibilityOf} from 'wix-ui-test-utils/protractor';
+import {headingTestkitFactory} from '../../testkit/protractor';
+
+describe('Heading', () => {
+  const storyUrl = getStoryUrl('Components', 'Heading');
+
+  beforeEach(() => browser.get(storyUrl));
+  eyes.it('should display correct content', () => {
+    const dataHook = 'storybook-heading';
+    const driver = headingTestkitFactory({dataHook});
+
+    return waitForVisibilityOf(driver.element(), 'Cannot find Heading')
+      .then(() => expect(driver.getTextContent()).toBe('hello'));
+  });
+});
