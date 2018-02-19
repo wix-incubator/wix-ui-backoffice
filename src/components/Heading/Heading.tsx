de8aa2078183b324caf217f128aff5a52b0e6ec9
Lior Belinsky <liorbe@wix.com>
2018-02-19 18:48:55 +0200

    create stylable Heading component (#40)
    * create tylable Heading component
    
    * refactor default props
    
    * pass the core props
    
    * pass props to stylable component
    
    * fix light colors
    
    * refactor types
    
    * fix css syntax
    
    * fix thin helvetica typography
    
    * refactor st.css states order
    
    * refactor story
    
    * Update Heading.tsx
    
    * Update index.tsx
    
    * update types
    
    * lint

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -1,72 +1,57 @@
 import * as React from 'react';
-import {oneOf, node} from 'prop-types';
-import {Text as CoreText, TextProps as CoreTextProps} from 'wix-ui-core/Text';
-import {ThemedComponent} from 'wix-ui-theme';
-import {theme, Skin, Appearance} from './theme';
+import {oneOf} from 'prop-types';
+import {Text as CoreText, TextProps as CoreTextProps} from 'wix-ui-core/StylableText';
+import style from './Heading.st.css';
+import {withStylable} from 'wix-ui-core/withStylable';
+import {Color} from '../../colors';
 
-interface HeadingProps extends CoreTextProps {
+export type Appearance = 'H1' | 'H2' | 'H3' | 'H4' | 'H5';
+type TagName = 'h1' | 'h2' | 'h3' | 'h4' | 'h5';
+export type Skin = 'dark' | 'light';
+
+interface Props {
   /** skin color of the heading */
   skin?: Skin;
 
   /** typography of the heading */
   appearance?: Appearance;
-
-  /** The text to show */
-  children?: React.ReactNode;
 }
 
-interface State {
-  tagName: string;
-}
+interface State { tagName: TagName; }
 
-export class Heading extends React.PureComponent<HeadingProps, State> {
+const defaultProps: Props = {
+  appearance: 'H1',
+  skin: 'dark'
+};
+
+const StyledText = withStylable<CoreTextProps, Props>(
+  CoreText,
+  style,
+  ({skin, appearance}) => ({[skin]: true, [appearance]: true}),
+  defaultProps
+);
+
+export class Heading extends React.PureComponent<Props, State> {
   static propTypes = {
+    ...CoreText.propTypes,
+
     /** skin color of the heading */
     skin: oneOf(['dark', 'light']),
 
     /** typography of the heading */
-    appearance: oneOf(['H0', 'H1', 'H2', 'H3']),
-
-    /** The text to show */
-    children: node
+    appearance: oneOf(['H1', 'H2', 'H3', 'H4', 'H5']),
   };
 
-  static defaultProps: Partial<HeadingProps> = {
-    appearance: 'H0',
-    skin: 'dark'
-  };
+  static defaultProps: Props = defaultProps;
 
-  constructor(props: HeadingProps) {
+  constructor(props: Props) {
     super(props);
-    this.state = {tagName: getType(props.appearance)};
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.appearance !== nextProps.appearance) {
-      this.setState({tagName: getType(nextProps.appearance)});
-    }
+    this.state = {tagName: (props.appearance.toLowerCase()) as TagName};
   }
 
   render() {
-    const {children, appearance, skin} = this.props;
-
     return (
-      <ThemedComponent {...{theme, appearance, skin}}>
-        <CoreText tagName={this.state.tagName}>
-          {children}
-        </CoreText>
-      </ThemedComponent>
+      <StyledText {...this.props} tagName={this.state.tagName}/>
     );
   }
-}
-
-function getType(appearance: Appearance) {
-  return [
-    {type: 'h1', candidates: ['H0']},
-    {type: 'h2', candidates: ['H1']},
-    {type: 'h3', candidates: ['H2']},
-    {type: 'h4', candidates: ['H3']}
-  ]
-    .filter(({candidates}) => candidates.indexOf(appearance) !== -1)
-    .reduceRight((acc, {type}) => type, 'span');
 }
