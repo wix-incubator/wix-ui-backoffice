abad7687859d540d4c11a505de5e326cd09b1e43
Lior Belinsky <liorbe@LiorBe-MBP15-2.local>
2018-02-19 21:40:29 +0200

    fix import paths

--- src/testkit/protractor.ts
+++ src/testkit/protractor.ts
@@ -3,5 +3,5 @@ import {protractorTestkitFactoryCreator} from 'wix-ui-test-utils/protractor';
 import {counterBadgeDriverFactory} from '../components/CounterBadge/CounterBadge.protractor.driver';
 export const counterBadgeTestkitFactory = protractorTestkitFactoryCreator(counterBadgeDriverFactory);
 
-import {headingDriverFactory} from '../components/Heading/Heading.driver';
+import {headingDriverFactory} from '../components/Heading/Heading.protractor.driver';
 export const headingTestkitFactory = protractorTestkitFactoryCreator(headingDriverFactory);
