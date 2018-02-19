66f629d5f0f6551ed67c03ecefadb9fda373a0b0
Daniel Strokovsky <danielstr@wix.com>
2018-02-19 15:43:35 +0200

    using icons instead of background image for ticks

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,20 +1,16 @@
 import {Checkbox as CoreCheckbox, CheckboxProps as CoreCheckboxProps} from 'wix-ui-core/Checkbox';
+import {CheckboxChecked, CheckboxIndeterminate} from 'wix-ui-icons-common/system/';
 import style from './Checkbox.st.css';
 import {withStylable} from 'wix-ui-core/withStylable';
 
-interface CheckboxProps {
-  skin?: 'standard' | 'error' | 'success';
-  size?: 'large' | 'medium';
-}
-
 const defaultProps = {
-  skin: 'standard',
-  size: 'large'
+  tickIcon: CheckboxChecked({width: '9px', height: '9px'}),
+  indeterminateIcon: CheckboxIndeterminate({width: '8px', height: '8px'})
 };
 
-export const Checkbox = withStylable<CoreCheckboxProps, CheckboxProps>(
+export const Checkbox = withStylable<CoreCheckboxProps, {}>(
   CoreCheckbox,
   style,
-  ({skin, size}) => ({[size]: true, [skin]: true}),
+  () => null,
   defaultProps
 );
