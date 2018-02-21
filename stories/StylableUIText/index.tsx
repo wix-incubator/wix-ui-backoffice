d3cb6d704994f294bad7dfd1c9b4c907f08a9ef8
Lior Belinsky <liorbe@wix.com>
2018-02-21 13:29:52 +0200

    Update index.tsx

--- stories/StylableUIText/index.tsx
+++ stories/StylableUIText/index.tsx
@@ -4,7 +4,7 @@ import {UIText} from '../../src/components/StylableUIText';
 
 export const story = () => storiesOf('Components', module)
   .add('StylableUIText', () => (
-    <UIText appearance="T3.5" data-hook="storybook-uiText">
+    <UIText appearance="T1.1" data-hook="storybook-uiText">
       Some text
     </UIText>
   ));
