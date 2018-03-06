1b49f30d2c4dbcea34d91a116e7d6cedd2c28bf5
Lior Belinsky <liorbe@LiorBe-MBP15-2.local>
2018-03-06 15:58:02 +0200

    add urgent skin to CounterBadge

--- src/components/StylableCounterBadge/constants.ts
+++ src/components/StylableCounterBadge/constants.ts
@@ -1,10 +1,11 @@
-export type Skin = 'default' | 'standard' | 'danger' | 'warning' | 'success';
+export type Skin = 'default' | 'standard' | 'danger' | 'warning' | 'urgent' | 'success';
 
 export enum SKIN {
   default = 'default',
   standard = 'standard',
   danger = 'danger',
   warning = 'warning',
+  urgent = 'urgent',
   success = 'success'
 }
 
