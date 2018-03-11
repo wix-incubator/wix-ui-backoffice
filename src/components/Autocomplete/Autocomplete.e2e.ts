4432a40640ce52304696924740b10da87a05cbbc
Amir Yonatan <amiry@wix.com>
2018-03-11 11:39:55 +0200

    changed e2e

--- src/components/Autocomplete/Autocomplete.e2e.ts
+++ src/components/Autocomplete/Autocomplete.e2e.ts
@@ -1,5 +1,5 @@
 import * as eyes from 'eyes.it';
-import {getStoryUrl} from 'wix-ui-test-utils/protractor';
+import {getStoryUrl, waitForVisibilityOf} from 'wix-ui-test-utils/protractor';
 import {autocompleteTestkitFactory} from '../../testkit/protractor';
 import {browser} from 'protractor';
 
@@ -9,9 +9,9 @@ describe('Autocomplete', () => {
 
   beforeEach(() => browser.get(storyUrl));
 
-  eyes.it('should render', () => {
+  eyes.it('should render autocomplete', async () => {
     const driver = autocompleteTestkitFactory({dataHook});
-
+    await waitForVisibilityOf(driver.element(), 'Cannot find Autocomplete');
     expect(driver.element()).toBeDefined();
   });
 });
