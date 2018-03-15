756bedededd61c72432bd185d7051a7ba951f821
Lior Belinsky <liorbe@LiorBe-MBP15-2.local>
2018-03-15 13:37:01 +0200

    improve ToggleSwitch story

--- stories/StylableToggleSwitch/index.tsx
+++ stories/StylableToggleSwitch/index.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import {ToggleSwitch} from '../../src/components/StylableToggleSwitch';
 import {SKIN} from '../../src/components/StylableCounterBadge/constants';
-import {Facebook} from 'wix-ui-icons-common';
+import {Music, VideoCamera} from 'wix-ui-icons-common';
 import {Autocomplete} from '../../src/components/Autocomplete';
 import {Heading} from '../../src/components/Heading';
 import {UIText} from '../../src/components/StylableUIText';
 
 const skinOptions = ['standard', 'error', 'success'].map(value => Autocomplete.createOption({id: value, value}));
 const sizeOptions = ['small', 'medium', 'large'].map(value => Autocomplete.createOption({id: value, value}));
+const checkedIconOptions = ['v', 'VideoCamera'].map(value => Autocomplete.createOption({id: value, value}));
+const uncheckedIconOptions = ['Minus', 'Music'].map(value => Autocomplete.createOption({id: value, value}));
 
 class ControlleToggleSwitchExample extends React.Component<any, any> {
   constructor(props) {
@@ -17,7 +19,9 @@ class ControlleToggleSwitchExample extends React.Component<any, any> {
       skin: 'standard',
       size: 'medium',
       checked: false,
-      disabled: false
+      disabled: false,
+      checkedIcon: 'v',
+      uncheckedIcon: 'Minus'
     };
   }
 
@@ -31,11 +35,11 @@ class ControlleToggleSwitchExample extends React.Component<any, any> {
           <Heading appearance="H3">checked: </Heading> <ToggleSwitch size="small" checked={this.state.checked} onChange={() => this.setState({checked: !this.state.checked})}/><br/><br/>
           <Heading appearance="H3">disabled: </Heading> <ToggleSwitch size="small" checked={this.state.disabled} onChange={() => this.setState({disabled: !this.state.disabled})}/><br/><br/>
           <Heading appearance="H3">tabIndex: </Heading> <UIText appearance="T2.4">Tab Index</UIText><br/><br/>
-          <Heading appearance="H3">onChange: </Heading> <UIText appearance="T2.4">Callback function when user changes the value of the component</UIText><br/><br/>
+          <Heading appearance="H3">onChange: </Heading> <UIText appearance="T2.4">Callback function when VideoCamera changes the value of the component</UIText><br/><br/>
           <Heading appearance="H3">styles: </Heading> <UIText appearance="T2.4">Inline styles for various parts of the switch</UIText><br/><br/>
           <Heading appearance="H3">id: </Heading> <UIText appearance="T2.4">The ID attribute to put on the toggle</UIText><br/><br/>
-          <Heading appearance="H3">checkedIcon: </Heading> <UIText appearance="T2.4">Icon inside of the knob when checked</UIText><br/><br/>
-          <Heading appearance="H3">uncheckedIcon: </Heading> <UIText appearance="T2.4">Icon inside of the knob when unchecked</UIText><br/><br/>
+          <Heading appearance="H3">checkedIcon: </Heading> <Autocomplete options={checkedIconOptions} onSelect={({value}) => this.setState({checkedIcon: value})} initialSelectedId={this.state.checkedIcon}/><br/><br/>
+          <Heading appearance="H3">uncheckedIcon: </Heading> <Autocomplete options={uncheckedIconOptions} onSelect={({value}) => this.setState({uncheckedIcon: value})} initialSelectedId={this.state.uncheckedIcon}/><br/><br/>
         </div>
         <div>
           <Heading> Preview </Heading><br/><br/><br/><br/><br/><br/><br/><br/><br/>
@@ -45,6 +49,8 @@ class ControlleToggleSwitchExample extends React.Component<any, any> {
             checked={this.state.checked}
             disabled={this.state.disabled}
             onChange={() => this.setState({checked: !this.state.checked})}
+            checkedIcon={this.state.checkedIcon == 'v' ? undefined : <VideoCamera/>}
+            uncheckedIcon={this.state.uncheckedIcon == 'Minus' ? undefined : <Music/>}
             dataHook="storybook-ToggleSwitch"
             />
         </div>
