dc23f93b710d8416a8ccc575e2d3f3eeac3b006f
Kieran Williams <kieranw@wix.com>
2018-03-21 16:25:14 +0200

    Ported VBox from wix-ui-core into backoffice

---
+++ src/components/VBox/coreVBox/theme.ts
@@ -0,0 +1,9 @@
+export type VBoxTheme = {
+ spacing?: string
+ horizontalAlignment?: 'left' | 'center' | 'right'
+};
+
+export const core: VBoxTheme = {
+  spacing: '20px',
+  horizontalAlignment: 'left'
+};
