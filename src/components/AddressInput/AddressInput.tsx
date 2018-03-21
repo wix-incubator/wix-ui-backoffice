71cecdc20e92397e0ab04dc9a57c47958d2e99c3
Amir Yonatan <amiry@wix.com>
2018-03-21 16:28:48 +0200

    added address input states

--- src/components/AddressInput/AddressInput.tsx
+++ src/components/AddressInput/AddressInput.tsx
@@ -3,6 +3,18 @@ import {AddressInput as CoreAddressInput, AddressInputProps as CoreAddressInputP
 import {withStylable} from 'wix-ui-core/withStylable';
 import style from './AddressInput.st.css';
 
-export const AddressInput = withStylable<CoreAddressInputProps>(
-      CoreAddressInput as React.ComponentClass<CoreAddressInputProps>,
-      style);
+export interface AddressInputProps {
+  skin?: 'standard' | 'error' | 'success';
+  size?: 'large' | 'medium' | 'small';
+}
+
+const defaultProps = {
+  skin: 'standard',
+  size: 'medium'
+};
+
+export const AddressInput = withStylable<CoreAddressInputProps, AddressInputProps>(
+  CoreAddressInput as React.ComponentClass<CoreAddressInputProps>,
+  style,
+  ({skin, size}) => ({size, skin}),
+  defaultProps);
