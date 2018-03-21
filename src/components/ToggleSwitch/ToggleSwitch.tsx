8ae58361db9f0cc1727b2ee966fcb04335d6c03a
Lior Belinsky <liorbe@wix.com>
2018-03-21 10:51:30 +0200

    Finish with BO ToggleSwitch (#62)
    * Finish with BO ToggleSwitch
    
    * update comments from cr
    
    * rename e2e test to .ts
    
    * fix dataHook in tests
    
    * update lodash import path
    
    * handle focus in test properly
    
    * remove uneeded tabindex
    
    * add files to remove them due to git bug
    
    * remove stylableToggle files
    
    * remove unused file
    
    * use protractor.key instead of selenium

--- src/components/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -1,42 +1,66 @@
 import * as React from 'react';
-import {ToggleSwitch as CoreToggleSwitch, ToggleSwitchProps as CoreToggleSwitchProps} from 'wix-ui-core/ToggleSwitch';
-import {ThemedComponent} from 'wix-ui-theme';
-import {oneOf, Requireable} from 'prop-types';
-import {ThemedComponentProps} from 'wix-ui-jss';
-import {theme, Size, Skin} from './theme';
-import {WixComponentProps} from 'wix-ui-core/dist/src/createHOC';
+import * as PropTypes from 'prop-types';
+import {
+  ToggleSwitch as CoreToggleSwitch,
+  ToggleSwitchProps as CoreToggleSwitchProps
+} from 'wix-ui-core/StylableToggleSwitch';
+import style from './ToggleSwitch.st.css';
+import {Skin, Size, SKINS, SIZES} from './constants';
+import {ToggleOff, ToggleOn, SmallToggleOff, SmallToggleOn} from 'wix-ui-icons-common/system';
+import {withStylable} from 'wix-ui-core/withStylable';
+import * as omit from 'lodash/omit';
 
-export interface ToggleSwitchProps extends CoreToggleSwitchProps {
-  /** size of the toggle switch */
-  size?: Size;
-
-  /** Color for disabled toggle switch */
+export interface ToggleSwitchProps {
   skin?: Skin;
+  size?: Size;
 }
 
-export class ToggleSwitch extends React.PureComponent<ToggleSwitchProps>  {
+const defaultProps = {
+  skin: SKINS.standard,
+  size: SIZES.large
+};
+
+const checkedIconMap = {
+  [SIZES.small]: undefined,
+  [SIZES.medium]: <SmallToggleOn/>,
+  [SIZES.large]: <ToggleOn/>
+};
+
+const uncheckedIconMap = {
+  [SIZES.small]: undefined,
+  [SIZES.medium]: <SmallToggleOff/>,
+  [SIZES.large]: <ToggleOff/>
+};
+
+const StyledToggleSwitch = withStylable<CoreToggleSwitchProps, ToggleSwitchProps>(
+  CoreToggleSwitch,
+  style,
+  ({size, skin}) => ({size, skin}),
+  defaultProps
+);
+
+const {checkedIcon, uncheckedIcon, styles, ...legalCorePropTypes} = CoreToggleSwitch.propTypes;
+
+export class ToggleSwitch extends React.PureComponent<ToggleSwitchProps & CoreToggleSwitchProps> {
   static propTypes = {
-    ...CoreToggleSwitch.propTypes,
-
-    /** size of the toggle switch */
-    size: oneOf(['x-small', 'small', 'large']),
-
-    /** Color for disabled toggle switch */
-    skin: oneOf(['standard', 'error', 'success'])
+    ...legalCorePropTypes,
+    /** Size of the ToggleSwitch */
+    size: PropTypes.oneOf(Object.keys(SIZES)),
+    /** Skin of the ToggleSwitch */
+    skin: PropTypes.oneOf(Object.keys(SKINS))
   };
 
-  static defaultProps: Partial<ToggleSwitchProps> = {
-    size: 'large',
-    skin: 'standard'
-  };
+  static defaultProps = defaultProps;
 
   render() {
-    const {size, skin, ...coreProps} = this.props;
+    const desiredProps = omit(this.props, 'styles');
 
     return (
-      <ThemedComponent {...{theme, size, skin}}>
-        <CoreToggleSwitch {...coreProps}/>
-      </ThemedComponent>
+      <StyledToggleSwitch
+        {...desiredProps}
+        checkedIcon={checkedIconMap[this.props.size]}
+        uncheckedIcon={uncheckedIconMap[this.props.size]}
+        />
     );
   }
 }
