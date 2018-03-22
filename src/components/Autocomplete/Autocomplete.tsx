04d7296beaa801ed4ad3390191d15d100d99dc9d
Amir Yonatan <amiry@wix.com>
2018-03-22 18:59:02 +0200

    refactor autocomplete

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -1,31 +1,26 @@
 import * as React from 'react';
-import {string} from 'prop-types';
 import {Autocomplete as CoreAutocomplete, AutocompleteProps as CoreAutocompleteProps} from 'wix-ui-core/Autocomplete';
+import {withStylable} from 'wix-ui-core/withStylable';
 import style from './Autocomplete.st.css';
-import ArrowDown from 'wix-ui-icons-common/ArrowDown';
 
 export interface AutocompleteProps {
   size?: 'large' | 'medium' | 'small';
 }
+
+const defaultProps = {
+  size: 'medium'
+};
 
 export type AutocompleteType = React.SFC<CoreAutocompleteProps & AutocompleteProps> & {
   createOption: typeof CoreAutocomplete.createOption;
   createDivider: typeof CoreAutocomplete.createDivider;
 };
 
-export const Autocomplete: AutocompleteType = ((props: CoreAutocompleteProps & AutocompleteProps) => {
-  const {size, ...otherProps} = props;
-  return (
-    <CoreAutocomplete
-      {...style('root', {size: props.size}, props)}
-      {...otherProps}
-      ArrowIcon={<ArrowDown className={style.arrowIcon} />}
-    />
-  );
-}) as AutocompleteType;
+export const Autocomplete = withStylable<CoreAutocompleteProps, AutocompleteProps>(
+  CoreAutocomplete,
+  style,
+  ({size}) => ({size}),
+  defaultProps) as AutocompleteType;
 
 Autocomplete.createOption = CoreAutocomplete.createOption;
 Autocomplete.createDivider = CoreAutocomplete.createDivider;
-Autocomplete.displayName = CoreAutocomplete.displayName;
-Autocomplete.propTypes = Object.assign({size: string}, CoreAutocomplete.propTypes);
-Autocomplete.defaultProps = {size: 'medium'};
