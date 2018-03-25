6d2b94dee779cc4b7b82203a9bf0621b6b76937b
Alexey Lebedev <alisey@users.noreply.github.com>
2018-03-25 11:09:15 +0300

    Checkbox: use wix-ui-icons-common (#71)
    Checkbox: use icons from wix-ui-icons-common

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Checkbox as CoreCheckbox, CheckboxProps as CoreCheckboxProps} from 'wix-ui-core/Checkbox';
 import style from './Checkbox.st.css';
 import {withStylable} from 'wix-ui-core/withStylable';
+import {CheckboxChecked, CheckboxIndeterminate} from 'wix-ui-icons-common/system';
 
 export interface CheckboxProps {
   skin?: 'standard' | 'error' | 'success';
@@ -10,7 +11,9 @@ export interface CheckboxProps {
 
 const defaultProps = {
   skin: 'standard',
-  size: 'large'
+  size: 'large',
+  checkedIcon: <CheckboxChecked />,
+  indeterminateIcon: <CheckboxIndeterminate />
 };
 
 export const Checkbox = withStylable<CoreCheckboxProps, CheckboxProps>(
