83b208d2ba6ae254fb56988e69b93cee2fabd040
Amir Yonatan <amiry@wix.com>
2018-03-25 12:10:21 +0300

    Added exclamation icon

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -2,10 +2,15 @@ import * as React from 'react';
 import {Autocomplete as CoreAutocomplete, AutocompleteProps as CoreAutocompleteProps} from 'wix-ui-core/Autocomplete';
 import {withStylable} from 'wix-ui-core/withStylable';
 import ArrowDown from 'wix-ui-icons-common/ArrowDown';
+import Exclamation from 'wix-ui-icons-common/Exclamation';
 import style from './Autocomplete.st.css';
+import {Tooltip} from '../Tooltip';
 
 export interface AutocompleteProps {
+  // The size of the autocomplete
   size?: 'large' | 'medium' | 'small';
+  /** The error message */
+  errorMessage?: string;
 }
 
 const defaultProps = {
@@ -23,11 +28,29 @@ export type AutocompleteType = React.SFC<CoreAutocompleteProps & AutocompletePro
   createDivider: typeof CoreAutocomplete.createDivider;
 };
 
+const renderSuffix = ({error, errorMessage}) => {
+  if (error) {
+    const errorIcon = <div className={style.errorIcon}><Exclamation /></div>;
+    if (!errorMessage) {
+      return errorIcon;
+    }
+
+    return (
+      <Tooltip content={errorMessage}>
+        {errorIcon}
+      </Tooltip>
+    );
+  }
+
+  return <ArrowDown className={style.arrowIcon} />;
+};
+
 export const Autocomplete: AutocompleteType =
   ((props: CoreAutocompleteProps & AutocompleteProps) => {
+    const {error, errorMessage} = props;
     return (
       <StyledAutocomplete
-        suffix={<ArrowDown className={style.arrowIcon} />}
+        suffix={renderSuffix({error, errorMessage})}
         {...props}
       />
     );
