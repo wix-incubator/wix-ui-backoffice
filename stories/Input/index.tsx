2a7ed8c16f39be971e036c48474bc872aa7740fb
Arijus Šukys <argshook@gmail.com>
2018-03-28 11:43:35 +0300

    docs(Input): use autodocs (#74)

--- stories/Input/index.tsx
+++
@@ -1,9 +0,0 @@
-import createStory from '../create-story';
-import * as React from 'react';
-import {storiesOf} from '@storybook/react';
-import {Input} from '../../src/components/Input';
-
-export const story = () => storiesOf('Components', module)
-  .add('Input', () => (
-      <Input data-hook="storybook-input"/>
-  ));
