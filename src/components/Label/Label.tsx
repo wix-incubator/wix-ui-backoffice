4a813fc7fe2dc275bb528ded4dd9f3cc8c364522
Kieran Williams <kieranw@wix.com>
2018-04-11 16:12:10 +0300

    Removed unneeded appearance types

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -3,12 +3,7 @@ import {Label as CoreLabel, LabelProps as CoreLabelProps} from 'wix-ui-core/Labe
 import style from './Label.st.css';
 import {withStylable} from 'wix-ui-core/withStylable';
 
-export type Appearance =
-    'T1' | 'T1.1' | 'T1.2' | 'T1.3' | 'T1.4' | 'T1.5' | 'T1.6' | 'T1.7' |
-    'T2' | 'T2.1' | 'T2.2' | 'T2.3' | 'T2.4' |
-    'T3' | 'T3.1' | 'T3.2' | 'T3.3' | 'T3.4' | 'T3.5' | 'T3.6' | 'T3.7' |
-    'T4' | 'T4.1' | 'T4.2' | 'T4.3' | 'T4.4' | 'T4.5' | 'T4.6' | 'T4.7' |
-    'T5' | 'T5.1';
+export type Appearance = 'T1.1' | 'T3.1';
 
 export interface LabelProps {
   /** typography of the label */
