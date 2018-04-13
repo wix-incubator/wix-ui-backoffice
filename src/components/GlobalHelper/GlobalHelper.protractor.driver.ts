50e67d4049c6bb3c990a018dd17ec357150b4051
Erez Makavy <erezm@wix.com>
2018-04-13 21:53:56 +0300

    Add HelperContent. (and master PR fixes)

--- src/components/GlobalHelper/GlobalHelper.protractor.driver.ts
+++ src/components/GlobalHelper/GlobalHelper.protractor.driver.ts
@@ -1,3 +1,12 @@
+import {$, ElementFinder, by} from 'protractor';
 import {popoverDriverFactory} from 'wix-ui-core/dist/src/baseComponents/Popover/Popover.protractor.driver';
+import {DataHooks} from './DataHooks';
+import {helperContentDriverFactory} from './content/HelperContent.protractor.driver';
+import {dataHookLocator} from '../../../test/utils/protractor';
 
-export const globalHelperDriverFactory = popoverDriverFactory;
+export const globalHelperDriverFactory = (element: ElementFinder) => {
+  return {
+    ...popoverDriverFactory(element),
+    getHelperContentDriver: () =>  helperContentDriverFactory(element.$(dataHookLocator(DataHooks.innerContent))),
+  };
+};
