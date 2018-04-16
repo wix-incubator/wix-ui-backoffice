1f20c5a05e0f6f2584a1ce009cc8f1f06f49e249
Erez Makavy <erezm@wix.com>
2018-04-16 08:18:00 +0300

    Add HelperContent Text

--- src/components/GlobalHelper/GlobalHelper.e2e.ts
+++ src/components/GlobalHelper/GlobalHelper.e2e.ts
@@ -1,7 +1,7 @@
 import * as eyes from 'eyes.it';
 import {browser, ElementFinder} from 'protractor';
 import {getStoryUrl, waitForVisibilityOf} from 'wix-ui-test-utils/protractor';
-
+import autoExampleDriver from 'wix-storybook-utils/AutoExampleDriver';
 import {globalHelperTestkitFactory} from '../../testkit/protractor';
 import {storySettings} from './../../../stories/GlobalHelper/StorySettings';
 
@@ -12,11 +12,18 @@ describe('GlobalHelper', () => {
 
     beforeEach(async () => {
         await browser.get(storyUrl);
+        await autoExampleDriver.reset();
         await waitForVisibilityOf(driver.element(), 'Cannot find GlobalHelper');
     });
 
-    eyes.it('should display title', async () => {
+    eyes.it('should display title and text', async () => {
         expect(await driver.getHelperContentDriver().hasTitle()).toBeTruthy();
-        expect(await driver.getHelperContentDriver().getTitleText()).toBe('This is the title');
+        expect(await driver.getHelperContentDriver().getTitleContent()).toBe('This is the title');
+        expect(await driver.getHelperContentDriver().getTextContent()).toBe('This is the a long text which is passed in the `text` propterty');
+    });
+
+    eyes.it('should display text only', async () => {
+        expect(await driver.getHelperContentDriver().hasTitle()).toBeFalsy();
+        expect(await driver.getHelperContentDriver().getTextContent()).toBe('This is the a long text which is passed in the `text` propterty');
     });
 });
