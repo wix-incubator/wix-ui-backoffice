1d180d8ebd453ad5920972568117be12ebc03144
Erez Makavy <erezm@wix.com>
2018-04-17 23:46:07 +0300

    Remove protractor util dataHookLocator

--- src/components/GlobalHelper/GlobalHelper.protractor.driver.ts
+++ src/components/GlobalHelper/GlobalHelper.protractor.driver.ts
@@ -2,11 +2,15 @@ import {$, ElementFinder, by} from 'protractor';
 import {popoverDriverFactory} from 'wix-ui-core/dist/src/baseComponents/Popover/Popover.protractor.driver';
 import {DataHooks} from './DataHooks';
 import {helperContentDriverFactory} from './content/HelperContent.protractor.driver';
-import {dataHookLocator} from '../../../test/utils/protractor';
 
 export const globalHelperDriverFactory = (element: ElementFinder) => {
   return {
     ...popoverDriverFactory(element),
-    getHelperContentDriver: () =>  helperContentDriverFactory(element.$(dataHookLocator(DataHooks.innerContent))),
+    getHelperContentDriver: () =>  helperContentDriverFactory(element.$(byDataHook(DataHooks.innerContent))),
   };
 };
+
+export function byDataHook(hook: string) {
+  return `[data-hook='${hook}']`;
+}
+
--- src/components/GlobalHelper/content/HelperContent.protractor.driver.ts
+++ src/components/GlobalHelper/content/HelperContent.protractor.driver.ts
@@ -1,14 +1,18 @@
 import {ElementFinder} from 'protractor';
 import {DataHooks} from './DataHooks';
-import {dataHookLocator} from '../../../../test/utils/protractor';
 import {textDriverFactory} from '../../Text/Text.protractor.driver';
 
 export const helperContentDriverFactory = (element: ElementFinder) => {
-  const title = () => element.$(dataHookLocator(DataHooks.title));
-  const text = () => element.$(dataHookLocator(DataHooks.text));
+  const title = () => element.$(byDataHook(DataHooks.title));
+  const text = () => element.$(byDataHook(DataHooks.text));
+
   return {
     hasTitle: async () => title().isPresent(),
     getTitleContent: async () => textDriverFactory(title()).getText(),
     getTextContent: async () => textDriverFactory(text()).getText()
   };
 };
+
+export function byDataHook(hook: string) {
+  return `[data-hook='${hook}']`;
+}
