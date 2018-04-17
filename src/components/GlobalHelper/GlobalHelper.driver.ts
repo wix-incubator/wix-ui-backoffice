20d15f588f6893fddb50c58c0c173b479a20bba3
Erez Makavy <erezm@wix.com>
2018-04-18 00:00:15 +0300

    Add to GLobalHelper popover Driver.

--- src/components/GlobalHelper/GlobalHelper.driver.ts
+++ src/components/GlobalHelper/GlobalHelper.driver.ts
@@ -1,16 +1,21 @@
 import {BaseDriver, DriverFactory} from 'wix-ui-test-utils/driver-factory';
+import {popoverDriverFactory} from 'wix-ui-core/dist/src/baseComponents/Popover/Popover.driver';
 import {DataHooks} from './DataHooks';
 import {helperContentDriverFactory, HelperContentDriver} from './content/HelperContent.driver';
 import {EnzymeDriverFactory} from 'wix-ui-test-utils/enzyme';
 
+// TODO: add interface of PopoverDriver
 export interface GlobalHelperDriver extends BaseDriver {
   getHelperContentDriver: () => HelperContentDriver;
 }
 
-export const globalHelperDriverFactory: DriverFactory<GlobalHelperDriver> | EnzymeDriverFactory<GlobalHelperDriver> = ({wrapper, element, eventTrigger}) => {
+export const globalHelperDriverFactory:
+  DriverFactory<GlobalHelperDriver> | EnzymeDriverFactory<GlobalHelperDriver> =
+  ({wrapper, element, eventTrigger}) => {
   const innerContent = () => element.querySelector(`[data-hook='${DataHooks.innerContent}']`);
 
   return {
+    ...popoverDriverFactory({element, eventTrigger}),
     /** Get HelperContent driver */
     getHelperContentDriver: () => helperContentDriverFactory({wrapper, element: innerContent(), eventTrigger}),
     /** checks if the component exists */
