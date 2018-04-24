db9ad0ffd267c8ba16ff23188f9db0874839a1a5
Erez Makavy <erezm@wix.com>
2018-04-24 15:04:57 +0300

    Add test for helperContent

--- src/components/FloatingHelper/FloatingHelper.spec.tsx
+++ src/components/FloatingHelper/FloatingHelper.spec.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import {floatingHelperDriverFactory, FloatingHelperDriver} from './FloatingHelper.driver';
-import {FloatingHelper} from './FloatingHelper';
+import {mount} from 'enzyme';
 import {createDriverFactory} from 'wix-ui-test-utils/driver-factory';
 import {isTestkitExists} from 'wix-ui-test-utils/vanilla';
 import {isEnzymeTestkitExists} from 'wix-ui-test-utils/enzyme';
 import {floatingHelperTestkitFactory} from '../../testkit';
 import {floatingHelperTestkitFactory as enzymeFloatingHelperTestkitFactory} from '../../testkit/enzyme';
-import {mount} from 'enzyme';
+import {floatingHelperDriverFactory, FloatingHelperDriver} from './FloatingHelper.driver';
+import {FloatingHelper} from './FloatingHelper';
+import {HelperContent} from './content';
 
 describe('FloatingHelper', () => {
   const createDriver = createDriverFactory(floatingHelperDriverFactory);
@@ -15,9 +16,9 @@ describe('FloatingHelper', () => {
     <FloatingHelper
       shown={true}
       placement="right"
-      content="Hello"
+      content={<HelperContent title="my title"/>}
     >
-      <div />
+      <div>This is the target element</div>
     </FloatingHelper>
   );
 
@@ -25,6 +26,12 @@ describe('FloatingHelper', () => {
   xit('should have arrow by default', () => {
     const driver = createDriver(element);
     // expect(driver.hasArrow()).toBe(true);
+  });
+
+  it('should have helper content (with title)', () => {
+    const driver = createDriver(element);
+    expect(driver.getHelperContentDriver().exists()).toBeTruthy();
+    expect(driver.getHelperContentDriver().getTitleContent()).toBe('my title');
   });
 
   describe('testkit', () => {
