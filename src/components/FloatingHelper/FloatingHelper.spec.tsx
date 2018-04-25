ad56cfe72d5dc64d6c1c4b67a5b6bd7c1e315281
Erez Makavy <erezm@wix.com>
2018-04-25 10:20:10 +0300

    Refactor : add buildComponent function

--- src/components/FloatingHelper/FloatingHelper.spec.tsx
+++ src/components/FloatingHelper/FloatingHelper.spec.tsx
@@ -6,43 +6,56 @@ import {isEnzymeTestkitExists} from 'wix-ui-test-utils/enzyme';
 import {floatingHelperTestkitFactory} from '../../testkit';
 import {floatingHelperTestkitFactory as enzymeFloatingHelperTestkitFactory} from '../../testkit/enzyme';
 import {floatingHelperDriverFactory, FloatingHelperDriver} from './FloatingHelper.driver';
-import {FloatingHelper} from './FloatingHelper';
-import {HelperContent} from '../../components/FloatingHelper/HelperContent';
+import {FloatingHelper, FloatingHelperProps} from './FloatingHelper';
+import {HelperContent, HelperContentProps} from '../../components/FloatingHelper/HelperContent';
+import defaults = require('lodash/defaults');
 
 describe('FloatingHelper', () => {
   const createDriver = createDriverFactory(floatingHelperDriverFactory);
 
-  const element = (
-    <FloatingHelper
-      shown
-      placement="right"
-      content={<HelperContent title="my title"/>}
-    >
-      <div>This is the target element</div>
-    </FloatingHelper>
-  );
+  const buildComponent = (props?: Partial<FloatingHelperProps>) => {
+    const defaultProps: FloatingHelperProps = {
+      shown: true,
+      placement: "right",
+      content: <HelperContent title="my title"/>,
+      children: <div>This is the target element</div>
+    };
+
+    return <FloatingHelper {...defaults({}, props, defaultProps)}/>;
+  };
 
   // Skipped: need to add hasArrow() method to Popover driver.
   xit('should have arrow by default', () => {
-    const driver = createDriver(element);
+    const driver = createDriver(buildComponent());
     // expect(driver.hasArrow()).toBe(true);
   });
 
   it('should have helper content (with title)', () => {
-    const driver = createDriver(element);
+    const driver = createDriver(buildComponent());
     expect(driver.getHelperContentDriver().exists()).toBeTruthy();
     expect(driver.getHelperContentDriver().getTitleContent()).toBe('my title');
   });
 
+  describe('close-button', () => {
+    it('should have a close-button by default', () => {
+      const driver = createDriver(buildComponent());
+      expect(driver.hasCloseButton()).toBeTruthy();
+    });
+    it('should NOT have a close-button', () => {
+      const driver = createDriver(buildComponent({withCloseButton: flase}));
+      expect(driver.hasCloseButton()).toBeFalsy();
+    });
+  });
+
   describe('testkit', () => {
     it('should exist', () => {
-      expect(isTestkitExists(element, floatingHelperTestkitFactory)).toBe(true);
+      expect(isTestkitExists(buildComponent(), floatingHelperTestkitFactory)).toBe(true);
     });
   });
 
   describe('enzyme testkit', () => {
     it('should exist', () => {
-      expect(isEnzymeTestkitExists(element, enzymeFloatingHelperTestkitFactory, mount)).toBe(true);
+      expect(isEnzymeTestkitExists(buildComponent(), enzymeFloatingHelperTestkitFactory, mount)).toBe(true);
     });
   });
 });
