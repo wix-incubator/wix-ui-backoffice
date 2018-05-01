94329b34f77502d677e68f31718103a9c59a8c47
Erez Makavy <erezm@wix.com>
2018-05-01 22:15:54 +0300

    Change HelperContent spec to be by feature

--- src/components/FloatingHelper/HelperContent/HelperContent.spec.tsx
+++ src/components/FloatingHelper/HelperContent/HelperContent.spec.tsx
@@ -1,22 +1,35 @@
 import * as React from 'react';
-import {helperContentDriverFactory} from './HelperContent.driver';
-import {HelperContent} from '.';
-import {createDriverFactory} from 'wix-ui-test-utils/driver-factory';
+import { helperContentDriverFactory } from './HelperContent.driver';
+import { HelperContent } from '.';
+import { createDriverFactory } from 'wix-ui-test-utils/driver-factory';
 
 describe('HelperContent', () => {
   const createDriver = createDriverFactory(helperContentDriverFactory);
 
-  it('should be empty by default', () => {
-    const driver = createDriver(<HelperContent/>);
-    expect(driver.hasTitle()).toBeFalsy();
-    expect(driver.hasBody()).toBeFalsy();
+  describe('title prop', () => {
+    it('should not have title by default', () => {
+      const driver = createDriver(<HelperContent />);
+      expect(driver.hasTitle()).toBeFalsy();
+    });
+
+    it('should have title with proper content', () => {
+      const driver = createDriver(<HelperContent title="title"/>);
+      expect(driver.hasTitle()).toBeTruthy();
+      expect(driver.getTitleContent()).toBe('title');
+    });
   });
 
-  it('should have title and text with proper values', () => {
-    const driver = createDriver(<HelperContent title="title" body="body"/>);
-    expect(driver.hasTitle()).toBeTruthy();
-    expect(driver.hasBody()).toBeTruthy();
-    expect(driver.getTitleContent()).toBe('title');
-    expect(driver.getBodyContent()).toBe('body');
+  describe('body prop', () => {
+    it('should not have body by default', () => {
+      const driver = createDriver(<HelperContent />);
+      expect(driver.hasTitle()).toBeFalsy();
+    });
+
+    it('should have body with simple text', () => {
+      const driver = createDriver(<HelperContent body="body" />);
+      expect(driver.hasBody()).toBeTruthy();
+      expect(driver.getBodyContent()).toBe('body');
+    });
   });
+
 });
