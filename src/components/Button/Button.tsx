edc3b260741fc19fa901886508f994f3c75f54f9
Erez Makavy <erezm@wix.com>
2018-05-01 16:45:02 +0300

    Add initial props: skin, priority and size

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,42 @@
 import * as React from 'react';
+import { string } from "prop-types";
 import {Button as CoreButton, ButtonProps as CoreButtonProps} from 'wix-ui-core/Button';
 import style from './Button.st.css';
 import {withStylable} from 'wix-ui-core/withStylable';
 
-export interface ButtonProps extends CoreButtonProps {}
+export interface ButtonOwnProps extends CoreButtonProps {
+  skin?: 'standard';
+  priority?: 'primary';
+  size?: 'medium';
+}
 
-export const Button = withStylable<CoreButtonProps, {}>(
-  CoreButton,
-  style,
-  () => null
-);
+const defaultProps :Partial<CoreButtonProps & ButtonOwnProps> ={
+  skin: 'standard',
+  priority: 'primary',
+  size: 'medium'
+}
+
+export type ButtonProps = ButtonOwnProps & CoreButtonProps;
+
+export const Button : React.SFC<ButtonProps> = props => {
+  const {children, skin, priority, size, ...rest} = props;
+
+  return (
+    <CoreButton 
+      {...rest}
+      {...style('root',{skin,priority,size}, rest)}
+    >
+      {children}
+    </CoreButton>
+  )
+}
+
+Button.defaultProps = defaultProps;
+
+Button.propTypes =  {
+  skin: string,
+  priority: string,
+  size: string
+}
+
+
