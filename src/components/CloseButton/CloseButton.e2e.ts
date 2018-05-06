5c25f8402e9f7b80b8fdb8d751ca7a3e3500bd66
Erez Makavy <erezm@wix.com>
2018-05-06 14:09:04 +0300

    Fix CloseButton e2e test. kind should be Internal and not COmponents

--- src/components/CloseButton/CloseButton.e2e.ts
+++ src/components/CloseButton/CloseButton.e2e.ts
@@ -1,16 +1,18 @@
 import * as eyes from 'eyes.it';
-import {browser} from 'protractor';
-import {getStoryUrl} from 'wix-ui-test-utils/protractor';
-import {closeButtonTestkitFactory} from '../../testkit/protractor';
+import { browser } from 'protractor';
+import { waitForVisibilityOf, createStoryUrl } from 'wix-ui-test-utils/protractor';
+import { closeButtonTestkitFactory } from '../../testkit/protractor';
+import { storySettings } from '../../../stories/CloseButton/storySettings';
 
 describe('CloseButton', () => {
-  const storyUrl = getStoryUrl('Components', 'CloseButton');
+  const storyUrl = createStoryUrl(storySettings);
   const dataHook = 'storybook-close-button';
 
   beforeEach(() => browser.get(storyUrl));
 
   eyes.it('should render with default props', async () => {
-    const driver = closeButtonTestkitFactory({dataHook});
+    const driver = closeButtonTestkitFactory({ dataHook });
+    await waitForVisibilityOf(driver.element());
     expect(await driver.element().isPresent()).toBeTruthy();
   });
 
---
+++ stories/CloseButton/storySettings.ts
@@ -0,0 +1,7 @@
+import { StorySettings } from '../storyTypes';
+
+export const storySettings: StorySettings = {
+  kind: 'Internal',
+  story: 'CloseButton',
+  dataHook: 'storybook-close-button'
+}
