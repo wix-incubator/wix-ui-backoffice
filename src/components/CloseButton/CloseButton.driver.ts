83c772e21b079ed2cb3cad545778cd6350a92140
Erez Makavy <erezm@wix.com>
2018-05-06 11:59:20 +0300

    remove CoreButton driver type, take it from core.

--- src/components/CloseButton/CloseButton.driver.ts
+++ src/components/CloseButton/CloseButton.driver.ts
@@ -1,35 +1,21 @@
 import { StylableDOMUtil } from 'stylable/test-utils';
-import { buttonDriverFactory as coreButtonDriverFactory } from 'wix-ui-core/dist/src/components/Button/Button.driver';
+import { buttonDriverFactory, ButtonDriver } from 'wix-ui-core/dist/src/components/Button/Button.driver';
 import { DriverFactory, BaseDriver, ComponentFactory } from 'wix-ui-test-utils/driver-factory';
 import style from './CloseButton.st.css';
 import { Skin, Size } from './constants';
 
-// TODO: move this to core
-export interface CoreButtonDriver extends BaseDriver {
-  exists: () => boolean;
-  click: () => any;
-  getType: () => any;
-  getTextContent: () => any;
-  isDisabled: () => boolean;
-  styles: {
-    getMinWidth: () => string;
-    getWidth: () => string;
-    getHeight: () => string;
-    getPadding: () => string;
-    getBorderRadius: () => string;
-  }
-}
-
-export interface CloseButtonDriver extends CoreButtonDriver {
+export interface CloseButtonDriver extends ButtonDriver {
   getSkin: () => Skin;
   getSize: () => Size;
 }
 
-export const closeButtonDriverFactory = ({ element, eventTrigger }: ComponentFactory): CloseButtonDriver => {
+export const closeButtonDriverFactory = (factoryParams: ComponentFactory): CloseButtonDriver => {
+  const { element } = factoryParams;
   const stylableDOMUtil = new StylableDOMUtil(style, element);
-  const getStyleState = <T>(stateName: string) => stylableDOMUtil.getStyleState(element, stateName) as any as T;
+  const getStyleState = <T>(stateName: string) => stylableDOMUtil.getStyleState(element, stateName) as any as T | null;
+
   return {
-    ...coreButtonDriverFactory({ element, eventTrigger }),
+    ...buttonDriverFactory(factoryParams),
     getSkin: () => getStyleState<Skin>('skin'),
     getSize: () => getStyleState<Size>('size'),
   };
