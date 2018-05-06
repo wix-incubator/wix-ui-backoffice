980644ecdf600a940148aafeb4e48b344215b361
Erez Makavy <erezm@wix.com>
2018-05-06 12:58:51 +0300

    FloatingHelper - Use closeButton

--- src/components/FloatingHelper/FloatingHelper.tsx
+++ src/components/FloatingHelper/FloatingHelper.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import pick = require('lodash/pick');
-import {bool, string, number, oneOfType} from 'prop-types';
+import { bool, string, number, oneOfType } from 'prop-types';
 import * as classnames from 'classnames';
-import {Popover, PopoverProps} from 'wix-ui-core/Popover';
-import {withStylable} from 'wix-ui-core/withStylable';
+import { Popover, PopoverProps } from 'wix-ui-core/Popover';
+import { withStylable } from 'wix-ui-core/withStylable';
 import style from './FloatingHelper.st.css';
-import {DataHooks} from './DataHooks';
-import {Button} from '../Button';
-import {Close as CloseIcon} from 'wix-ui-icons-common/system';
+import { DataHooks } from './DataHooks';
+import { Button } from '../Button';
+import { CloseButton } from '../CloseButton';
+import { Close as CloseIcon } from 'wix-ui-icons-common/system';
+import { Size, Skin } from '../CloseButton/constants';
 /**
  * Adapts Popover API with Popover.Element, and Popover.Content into regular props
  */
@@ -28,7 +30,7 @@ export interface FloatingHelperOwnProps {
 }
 
 const PickedPopoverPropTypes = pick(Popover.propTypes, 'placement', 'shown', 'moveBy', 'hideDelay', 'showDelay', 'appendTo', 'timeout', 'className');
-export type PickedPopoverProps = Pick<PopoverProps,    'placement'| 'shown'| 'moveBy'| 'hideDelay'| 'showDelay'| 'appendTo'| 'timeout' | 'className'>;
+export type PickedPopoverProps = Pick<PopoverProps, 'placement' | 'shown' | 'moveBy' | 'hideDelay' | 'showDelay' | 'appendTo' | 'timeout' | 'className'>;
 export type FloatingHelperProps = PickedPopoverProps & PopoverAdapterProps & FloatingHelperOwnProps;
 
 const FloatingHelperBO = withStylable<PopoverProps, {}>(
@@ -38,7 +40,7 @@ const FloatingHelperBO = withStylable<PopoverProps, {}>(
 );
 
 export const FloatingHelper: React.SFC<FloatingHelperProps> = props => {
-  const {children, content, width, ...rest} = props;
+  const { children, content, width, ...rest } = props;
   const contentWidth = (typeof width) === 'number' ? `${width}px` : width;
 
   return (
@@ -47,17 +49,16 @@ export const FloatingHelper: React.SFC<FloatingHelperProps> = props => {
       showArrow
     >
       <Popover.Element>
-          {children}
+        {children}
       </Popover.Element>
       <Popover.Content>
-        <div data-hook={DataHooks.contentWrapper} style={{width: contentWidth}}>
+        <div data-hook={DataHooks.contentWrapper} style={{ width: contentWidth }}>
           {props.showCloseButton && (
-            <Button
-              data-hook={DataHooks.closeButton}
-              className={classnames(style.closeButton, style.closeButtonWhiteSecondary)}
-            >
-              <CloseIcon className={style.closeIcon}/>
-            </Button>
+            <CloseButton
+              className={style.closeButton}
+              skin={Skin.white}
+              size={Size.large}
+            />
           )}
           <div data-hook={DataHooks.innerContent} className={style.innerContent}>
             {content}
