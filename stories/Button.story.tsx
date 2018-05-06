e5320a1df40caf180251d05ed7e97f3470a07c2e
Erez Makavy <erezm@wix.com>
2018-05-06 22:21:32 +0300

    Add all Button variation screenshots. Add examples.

---
+++ stories/Button.story.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { Button, ButtonProps, ButtonSkin as Skin, ButtonPriority as Priority, ButtonSize as Size } from '../src/components/Button';
+import * as ButtonSource from '!raw-loader!../src/components/Button/Button.tsx';
+
+
+function PButton(props: ButtonProps) {
+  return (
+    <div style={{ margin: '20px' }}>
+      <Button {...props}>
+        Click me!
+      </Button>
+    </div>
+  );
+}
+export default {
+  category: 'Components',
+  storyName: 'Button',
+  component: Button,
+  source: ButtonSource,
+  componentPath: '../src/components/Button/Button.tsx',
+  componentProps: setState => ({
+    'data-hook': 'storybook-button',
+    children: ['Click me!'],
+  }),
+  exampleProps: {
+    skin: ['standard', 'white']
+  },
+  examples: (
+    <div>
+      <h1>Examples</h1>
+      <h2>Primary</h2>
+      <PButton skin={Skin.standard} priority={Priority.primary} />
+      <PButton skin={Skin.white} priority={Priority.primary} />
+      <h2>Secondary</h2>
+      <PButton skin={Skin.standard} priority={Priority.secondary} />
+      <PButton skin={Skin.white} priority={Priority.secondary} />
+
+    </div>
+  )
+};
