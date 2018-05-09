e64bb0337405c15318972fd1ad166f041882f929
Alexey Lebedev <alisey@users.noreply.github.com>
2018-05-09 16:03:47 +0300

    Remove CounterBadge and UIText (#131)

--- src/components/CounterBadge/CounterBadge.driver.ts
+++
@@ -1,7 +0,0 @@
-import {badgeDriverFactory} from 'wix-ui-core/dist/src/components/Badge/Badge.driver';
-
-export const counterBadgeDriverFactory = ({element}) => {
-  return {
-    ...badgeDriverFactory({element})
-  };
-};
