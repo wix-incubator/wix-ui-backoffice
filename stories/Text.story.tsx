ae965ca8df85b3ec2773515dbe39f9d12faf9cff
argshook <argshook@gmail.com>
2018-05-10 17:52:18 +0300

    docs(Text): update story config & cleanup

--- stories/Text.story.tsx
+++ stories/Text.story.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
 import {Text} from '../src/components/Text';
 import {Heading} from '../src/components/Heading';
-import * as TextSource from '!raw-loader!../src/components/Text/Text.tsx';
 
 export default {
   category: 'Components',
   storyName: 'Text',
   component: Text,
-  source: TextSource,
   componentPath: '../src/components/Text/Text.tsx',
+  displayName: 'Text',
 
-  componentProps: setState => ({dataHook: 'storybook-text'}),
-  examples: 
+  componentProps: {
+    dataHook: 'storybook-text',
+    children: 'Some text'
+  },
+
+  examples: (
     <div>
       <Heading>Multiline Example: </Heading>
-      <Text>{`First line\nSecond line`}</Text>
+      <Text>{'First line\nSecond line'}</Text>
     </div>
+  )
 };
