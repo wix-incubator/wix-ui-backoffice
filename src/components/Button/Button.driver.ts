548fda733aabc77821faca9753ecbd6c3d8d1488
Daniel Strokovsky <danielstr@wix.com>
2018-05-10 10:12:49 +0300

    moved the button internals from wix-ui-core

--- src/components/Button/Button.driver.ts
+++ src/components/Button/Button.driver.ts
@@ -1,22 +1,53 @@
 import { StylableDOMUtil } from 'stylable/test-utils';
-import { buttonDriverFactory as coreButtonDriverFactory, ButtonDriver as CoreButtonDriver } from 'wix-ui-core/dist/src/components/Button/Button.driver';
 import { DriverFactory, BaseDriver, ComponentFactory } from 'wix-ui-test-utils/driver-factory';
 import style from './Button.st.css';
 import { Skin, Priority, Size } from './constants';
 
-export interface ButtonDriver extends CoreButtonDriver {
+export interface ButtonDriver extends BaseDriver {
+  /** click on the button root element */
+  click: () => any;
+  /** returns elements type attribute */
+  getType: () => any;
+  /** returns elements textContent */
+  getTextContent: () => any;
+  /** returns if the element is disabled */
+  isDisabled: () => boolean;
+  styles: {
+    /** returns elements min-width css property */
+    getMinWidth: () => string;
+    /** returns elements width css property */
+    getWidth: () => string;
+    /** returns elements height css property */
+    getHeight: () => string;
+    /** returns elements padding css property */
+    getPadding: () => string;
+    /** returns elements border-radius css property */
+    getBorderRadius: () => string;
+  }
   getSkin: () => Skin;
   getPriority: () => Priority;
   getSize: () => Size;
 }
 
 export const buttonDriverFactory: DriverFactory<ButtonDriver> = (factoryParams: ComponentFactory): ButtonDriver => {
-  const { element } = factoryParams;
+  const getButtonStyle = () => window.getComputedStyle(element);
+  const { element, eventTrigger } = factoryParams;
   const stylableDOMUtil = new StylableDOMUtil(style, element);
   const getStyleState = <T>(stateName: string) => stylableDOMUtil.getStyleState(element, stateName) as any as T | null;
 
   return {
-    ...coreButtonDriverFactory(factoryParams),
+    exists: () => !!element,
+    click: () => eventTrigger.click(element),
+    getType: () => element.getAttribute('type'),
+    getTextContent: () => element.textContent,
+    isDisabled: () => element.getAttribute('disabled') === '',
+    styles: {
+      getMinWidth: () => getButtonStyle().minWidth,
+      getWidth: () => getButtonStyle().width,
+      getHeight: () => getButtonStyle().height,
+      getPadding: () => getButtonStyle().padding,
+      getBorderRadius: () => getButtonStyle().borderRadius,
+    },
     getSkin: () => getStyleState<Skin>('skin'),
     getPriority: () => getStyleState<Priority>('priority'),
     getSize: () => getStyleState<Size>('size'),
--- src/components/Button/Button.protractor.driver.ts
+++ src/components/Button/Button.protractor.driver.ts
@@ -1,1 +1,25 @@
-export {buttonDriverFactory, ButtonDriver} from 'wix-ui-core/dist/src/components/Button/Button.protractor.driver';
+import {ElementFinder} from 'protractor';
+import {DriverFactory, BaseDriver} from 'wix-ui-core/dist/src/common/BaseDriver.protractor';
+
+export interface ButtonDriver extends BaseDriver {
+  /** returns true if the root element is present */
+  exists: () => Promise<boolean>;
+  /** returns the Button's text content */
+  getButtonTextContent: () => Promise<string>;
+  /** click the button */
+  click: () => Promise<void>;
+  /** checks wether the button is disabled */
+  isButtonDisabled: () => Promise<boolean>;
+}
+
+export const buttonDriverFactory: DriverFactory<ButtonDriver> = element => ({
+  element: () => element,
+  exists: async () => element.isPresent(),
+  getButtonTextContent: async () => element.getText(),
+  click: async () => element.click(),
+  isButtonDisabled: () => hasAttribute(element, 'disabled'),
+});
+
+// TODO: use this from wix-ui-test-utils, once it is added there
+const hasAttribute = async (elementFinder: ElementFinder, attributeName: string) =>
+  elementFinder.getAttribute(attributeName).then(value => value !== null);
