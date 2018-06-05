af7d3a5084ae27dbc4b5a40ff48a000aa19fbf8f
Erez Makavy <erezm@wix.com>
2018-06-05 14:20:24 +0300

    Remove commented code

--- stories/FloatingHelper/ControlledExample.tsx
+++ stories/FloatingHelper/ControlledExample.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-// import { FloatingHelper } from 'wix-ui-backoffice/FloatingHelper';
 import { FloatingHelper } from '../../src/components/FloatingHelper';
 
 export class ControlledExample extends React.Component {
