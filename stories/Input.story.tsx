2b12d0a0a71e58aae060398edb5e0c6a38f53488
argshook <argshook@gmail.com>
2018-06-12 11:49:09 +0300

    docs(Input): remove source
    it's not used

--- stories/Input.story.tsx
+++ stories/Input.story.tsx
@@ -1,11 +1,9 @@
 import {Input} from '../src/components/Input';
-import * as InputSource from '!raw-loader!../src/components/Input/Input.tsx';
 
 export default {
   category: 'Components',
   storyName: 'Input',
   component: Input,
-  source: InputSource,
   componentPath: '../src/components/Input/Input.tsx',
 
   componentProps: setState => ({
