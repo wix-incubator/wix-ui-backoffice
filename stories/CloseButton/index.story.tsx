0c95171c3149ba868febc5896d429089d1a257e5
argshook <argshook@gmail.com>
2018-06-13 15:57:29 +0300

    docs(CloseButton): cleanup

--- stories/CloseButton/index.story.tsx
+++ stories/CloseButton/index.story.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
+
 import { CloseButton } from '../../src/components/CloseButton';
-import * as CloseButtonSource from '!raw-loader!../../src/components/CloseButton/CloseButton.tsx';
 import { enumValues } from '../../src/utils';
 import { Skin, Size } from '../../src/components/CloseButton/constants';
 import { storySettings } from './storySettings';
@@ -9,12 +9,11 @@ export default {
   category: storySettings.kind,
   storyName: storySettings.story,
   component: CloseButton,
-  source: CloseButtonSource,
   componentPath: '../../src/components/CloseButton/CloseButton.tsx',
   componentProps: {
-    'data-hook': storySettings.dataHook,
+    'data-hook': storySettings.dataHook
   },
   exampleProps: {
-    size: enumValues(Size) // TODO: not shown in storybook?
+    size: enumValues(Size)
   }
 };
