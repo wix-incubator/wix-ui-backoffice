e0a8004182cd1e89efcee65cdf1f0bf18f08495d
Erez Makavy <erezm@wix.com>
2018-06-17 22:15:35 +0300

    `<Checkbox/>` - (fix) text-align should not affect checked icon position

--- src/components/Checkbox/Checkbox.e2e.ts
+++ src/components/Checkbox/Checkbox.e2e.ts
@@ -1,34 +1,58 @@
 import * as eyes from 'eyes.it';
-import {getStoryUrl} from 'wix-ui-test-utils/protractor';
+import {getStoryUrl, scrollToElement, mouseLeave} from 'wix-ui-test-utils/protractor';
 import {checkboxTestkitFactory} from 'wix-ui-core/dist/src/testkit/protractor';
 import {browser} from 'protractor';
+import autoExampleDriver = require('wix-storybook-utils/AutoExampleDriver');
 
 describe('Checkbox', () => {
   const storyUrl = getStoryUrl('Components', 'Checkbox');
   const dataHook = 'storybook-checkbox';
 
   beforeEach(() => browser.get(storyUrl));
-
-  eyes.it('should render', () => {
-    const driver = checkboxTestkitFactory({dataHook});
-
-    expect(driver.element()).toBeDefined();
+  afterEach(async () => {
+    await autoExampleDriver.reset();
   });
 
-  xit('should be unchecked and not disabled by default', () => {
-    const driver = checkboxTestkitFactory({dataHook});
+  describe('not hovered', () => {
+    eyes.it('should be unchecked', () => {
+      const driver = checkboxTestkitFactory({dataHook});
+      expect(driver.isChecked()).toBeFalsy();
+    });
+    
+    eyes.it('should be checked', async () => {
+      autoExampleDriver.setProps({checked: true});
+      const driver = checkboxTestkitFactory({dataHook});
+      await mouseLeave()
+      expect(await driver.isChecked()).toBeTruthy();
+    });
+    
+    eyes.it('should be indetermined', async () => {
+      autoExampleDriver.setProps({indeterminate: true});
+      const driver = checkboxTestkitFactory({dataHook});
+      await mouseLeave();
+      // TODO: implement driver.isIndeterminate() in wix-ui-core
+      // expect(await driver.isIndeterminate()).toBeTruthy();
+    });
 
-    expect(driver.isChecked()).toBe(false);
-    expect(driver.isDisabled()).toBe(false);
+    eyes.it('should display checked icon in place. not be affected by text-align', async () => {
+      const driver = checkboxTestkitFactory({dataHook: 'storybook-checkbox-text-align'});
+      await scrollToElement(driver.element());
+      await mouseLeave()
+      expect(await driver.isChecked()).toBeTruthy();
+    });
   });
 
-  xit('should become checked', () => {
+
+  it('should be unchecked and not disabled by default', async () => {
     const driver = checkboxTestkitFactory({dataHook});
+    expect(await driver.isChecked()).toBe(false);
+    expect(await driver.isDisabled()).toBe(false);
+  });
 
-    expect(driver.isChecked()).toBe(false);
-
-    driver.click();
-
-    expect(driver.isChecked()).toBe(true);
+  it('should be checked when clicked', async () => {
+    const driver = checkboxTestkitFactory({dataHook});
+    expect(await driver.isChecked()).toBe(false);
+    await driver.click();
+    expect(await driver.isChecked()).toBe(true);
   });
 });
