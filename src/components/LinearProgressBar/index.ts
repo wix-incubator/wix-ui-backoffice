fa145b002d9e8f0b6ca72f761331e202f98ae581
Aaron Vinestock <aaronvine@gmail.com>
2018-06-18 14:37:11 +0300

    adds data-hook support (#171)

--- src/components/LinearProgressBar/index.ts
+++ src/components/LinearProgressBar/index.ts
@@ -1,1 +1,4 @@
-export {LinearProgressBar} from './LinearProgressBar'
+import {LinearProgressBar as LinearProgressBarComponent} from './LinearProgressBar';
+import {createHOC} from 'wix-ui-core/dist/src/createHOC';
+
+export const LinearProgressBar = createHOC(LinearProgressBarComponent);
