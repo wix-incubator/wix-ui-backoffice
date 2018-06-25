ec35830a565d64408e6eb4cfd76fe8d37ed1643f
Lior Belinsky <liorbe@wix.com>
2018-06-25 14:56:16 +0300

    wip not working still

--- src/components/CircularProgressBar/CircularProgressBar.protractor.driver.ts
+++ src/components/CircularProgressBar/CircularProgressBar.protractor.driver.ts
@@ -1,1 +1,15 @@
-export {circularProgressBarDriverFactory, CircularProgressBarDriver} from 'wix-ui-core/dist/src/components/CircularProgressBar/CircularProgressBar.protractor.driver';
+import {$, ElementFinder} from 'protractor';
+import {DriverFactory} from 'wix-ui-core/dist/src/common/BaseDriver.protractor';
+export {circularProgressBarDriverFactory as coreCircularProgressBarDriverFactory, CircularProgressBarDriver as CoreCircularProgressBarDriver} from 'wix-ui-core/dist/src/components/CircularProgressBar/CircularProgressBar.protractor.driver';
+
+export interface CircularProgressBarDriver extends CoreCircularProgressBarDriver {
+  /** Get tooltip that appears on error icons */
+  getTooltip?: () => ElementFinder;
+}
+
+export const circularProgressBarDriverFactory: DriverFactory<CircularProgressBarDriver> = (element: ElementFinder): CircularProgressBarDriver => {
+  return {
+    ...coreCircularProgressBarDriverFactory($(`[data-hook='circular-progress-bar']`)),
+    getTooltip: () => (element.$(`[data-hook='tooltip']`)),
+  };
+};
