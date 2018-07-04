c1bd0f5ac40ece51d00861c1deca0b46de45952a
argshook <argshook@gmail.com>
2018-07-04 19:55:29 +0300

    chore(TimePicker): use `string` as value instead of date object
    ideally this component should support string, utc string and date object
    however, it requires changes in wix-ui-core
    moreover, first consumers of wix-ui-backoffice timepicker expect value
    to be string

--- src/components/TimePicker/TimePicker.spec.tsx
+++ src/components/TimePicker/TimePicker.spec.tsx
@@ -1,5 +1,6 @@
+/* global describe it expect */
+
 import * as React from 'react';
-import {format} from 'date-fns';
 
 import {timePickerDriverFactory} from './TimePicker.driver';
 import {TimePicker} from './';
@@ -11,89 +12,72 @@ import {timePickerTestkitFactory} from '../../testkit';
 import {timePickerTestkitFactory as enzymeTimePickerTestkitFactory} from '../../testkit/enzyme';
 
 describe('TimePicker', () => {
-    const createDriver = createDriverFactory(timePickerDriverFactory);
-    const format12Hours = date => format(date, 'hh:mm');
-    const format24Hours = date => format(date, 'HH:mm');
+  const createDriver = createDriverFactory(timePickerDriverFactory);
 
-    describe('time display', () => {
-        const defaultValue = new Date();
-        const defaultValueWithAM = new Date('2014-04-25T01:00:00.00Z');
-        const defaultValueWithPM = new Date('2014-04-25T13:00:00.00Z');
-
-        it(`should render the given default value`, () => {
-            const props = {
-                defaultValue
-            };
-            const driver = createDriver(<TimePicker {...props}/>);
-            expect(driver.getValue()).toBe(format12Hours(props.defaultValue));
-        });
-
-        it(`should render the current time if no default value were passed`, () => {
-            const driver = createDriver(<TimePicker/>);
-            const currentTime = defaultValue;
-            const currentTimeHours = format12Hours(currentTime).substring(0, 2);
-            const currentTimeMinutes = format12Hours(currentTime).substring(3, 5);
-            const inputTimeHours = driver.getValue().substring(0, 2);
-            const inputTimeMinutes = driver.getValue().substring(3, 5);
-            const minutesDiff = Math.abs((parseInt(inputTimeMinutes) - parseInt(currentTimeMinutes)));
-            expect(inputTimeHours).toBe(currentTimeHours);
-            expect(minutesDiff <= 1).toBeTruthy(); //ignore diff of one minute (minute can be change from the time the object was created to current time)
-        });
-
-        it(`should allow rendering time in 24 hours mode`, () => {
-            const props = {
-                defaultValue,
-                disableAmPm: true
-            };
-            const driver = createDriver(<TimePicker {...props}/>);
-            expect(driver.getValue()).toBe(format24Hours(props.defaultValue));
-        });
-
-        it(`should display am/pm indicator when in 12 hours mode`, () => {
-            const props = {
-                defaultValue,
-                disableAmPm: false
-            };
-            const driver = createDriver(<TimePicker {...props}/>);
-            expect(driver.isAmPmIndicatorExist()).toBeTruthy();
-        });
-
-        it(`should display AM indicator when in 12 hours mode and the time displayed is AM`, () => {
-            const props = {
-                defaultValue: defaultValueWithAM,
-                disableAmPm: false
-            };
-            const driver = createDriver(<TimePicker {...props}/>);
-            expect(driver.getAmPmIndicatorText()).toBe('am');
-        });
-
-        it(`should display AM indicator when in 12 hours mode and the time displayed is PM`, () => {
-            const props = {
-                defaultValue: defaultValueWithPM,
-                disableAmPm: false
-            };
-            const driver = createDriver(<TimePicker {...props}/>);
-            expect(driver.getAmPmIndicatorText()).toBe('pm');
-        });
+  describe('time display', () => {
+    it('should render given value', () => {
+      const driver = createDriver(<TimePicker value="12:34"/>);
+      expect(driver.getValue()).toBe('12:34');
     });
 
-    describe('block props', () => {
-        it('should not pass inline styles prop', () => {
-            const styles = {root: {color: 'green'}};
-            const wrapper = createDriver(<TimePicker styles={styles} size="small"/>);
-            expect(wrapper.getRootStyles().color).not.toBe('green');
-        });
+    it('should render --:-- when no value given', () => {
+      const driver = createDriver(<TimePicker/>);
+      expect(driver.getValue()).toBe('--:--');
     });
 
-    describe('testkit', () => {
-        it('should exist', () => {
-            expect(isTestkitExists(<TimePicker/>, timePickerTestkitFactory)).toBe(true);
-        });
+    it('should allow rendering time in 24 hours mode', () => {
+      const props = {
+        value: '23:59',
+        disableAmPm: true
+      };
+      const driver = createDriver(<TimePicker {...props}/>);
+      expect(driver.getValue()).toBe(props.value);
     });
 
-    describe('enzyme testkit', () => {
-        it('should exist', () => {
-            expect(isEnzymeTestkitExists(<TimePicker/>, enzymeTimePickerTestkitFactory, mount)).toBe(true);
-        });
+    it('should display am/pm indicator when in 12 hours mode', () => {
+      const props = {
+        value: '15:04',
+        disableAmPm: false
+      };
+      const driver = createDriver(<TimePicker {...props}/>);
+      expect(driver.isAmPmIndicatorExist()).toBeTruthy();
+      expect(driver.getValue()).toBe('03:04');
     });
+
+    it('should display AM indicator when in 12 hours mode and the time displayed is AM', () => {
+      const props = {
+        value: '11:59',
+        disableAmPm: false
+      };
+      const driver = createDriver(<TimePicker {...props}/>);
+      expect(driver.getAmPmIndicatorText()).toBe('am');
+    });
+
+    it('should display AM indicator when in 12 hours mode and the time displayed is PM', () => {
+      const props = {
+        value: '19:27',
+        disableAmPm: false
+      };
+      const driver = createDriver(<TimePicker {...props}/>);
+      expect(driver.getAmPmIndicatorText()).toBe('pm');
+    });
+  });
+
+  describe('`width` prop', () => {
+    // TODO: skipping for now, because it requires update in wix-ui-core TimePicker to support width prop too
+    it.skip('should not pass inline styles prop', () => {
+      const driver = createDriver(<TimePicker width="432px"/>);
+      expect(driver.getInputElement().style.width).toEqual('432px');
+    });
+  });
+
+  describe('testkits', () => {
+    it('should exist', () => {
+      expect(isTestkitExists(<TimePicker/>, timePickerTestkitFactory)).toBe(true);
+    });
+
+    it('should exist for enzyme', () => {
+      expect(isEnzymeTestkitExists(<TimePicker/>, enzymeTimePickerTestkitFactory, mount)).toBe(true);
+    });
+  });
 });
--- src/components/TimePicker/TimePicker.tsx
+++ src/components/TimePicker/TimePicker.tsx
@@ -1,78 +1,78 @@
 import * as React from 'react';
 import * as propTypes from 'prop-types';
 import omit = require('lodash/omit');
-import {format} from 'date-fns';
+
 import FormFieldSpinnerUp from 'wix-ui-icons-common/system/FormFieldSpinnerUp';
 import FormFieldSpinnerDown from 'wix-ui-icons-common/system/FormFieldSpinnerDown';
 
 import {
-    TimePicker as CoreTimePicker,
-    TimePickerProps as CoreTimePickerProps,
-    AmPmOptions
+  TimePicker as CoreTimePicker,
+  TimePickerProps as CoreTimePickerProps,
 } from 'wix-ui-core/TimePicker';
+import {AmPmOptions} from 'wix-ui-core/dist/src/components/TimePicker/constants';
 import style from './TimePicker.st.css';
 import {Size} from './constants';
 
 export interface TimePickerPropsExtended {
-    size?: Size,
-    value: Date;
-    disableAmPm?: boolean;
+  size?: Size,
+  value?: String;
+  disableAmPm?: boolean;
 }
 
 /*
-    'CoreTimePickerProps.useAmPm' prop must not be included in TimePickerProps.
-    Use type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>> to omit it from CoreTimePickerProps.
-    TODO: after typescript is updated to ^2.9 omit 'useAmPm' prop
+  'CoreTimePickerProps.useAmPm' prop must not be included in TimePickerProps.
+  Use type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>> to omit it from CoreTimePickerProps.
+  TODO: after typescript is updated to ^2.9 omit 'useAmPm' prop
  */
 export type TimePickerProps = CoreTimePickerProps & TimePickerPropsExtended;
 
 const defaultProps = {
-    size: Size.medium,
-    value: new Date(),
-    disableAmPm: false,
-    error: false,
-    tickerUpIcon: <FormFieldSpinnerUp/>,
-    tickerDownIcon: <FormFieldSpinnerDown/>,
+  size: Size.medium,
+  disableAmPm: false,
+  error: false,
+  tickerUpIcon: <FormFieldSpinnerUp/>,
+  tickerDownIcon: <FormFieldSpinnerDown/>,
 };
 
 function getInputWidthState(timePickerWidth: string | undefined, size: Size, disableAmPm: boolean): string {
-    if (timePickerWidth) {
-        return '';
-    }
+  if (timePickerWidth) {
+    return '';
+  }
 
-    const states: string[] = [size];
-    if (!disableAmPm) {
-        states.push('ampm');
-    }
+  const states: string[] = [size];
+  if (!disableAmPm) {
+    states.push('ampm');
+  }
 
-    return states.join('_');
+  return states.join('_');
 }
 
 
 
 export class TimePicker extends React.PureComponent<TimePickerProps> {
-    static defaultProps = defaultProps;
-    static propTypes = {
-        ...omit(CoreTimePicker.propTypes, 'useAmPm'),
-        size: propTypes.oneOf(Object.keys(Size)),
-        value: propTypes.instanceOf(Date).isRequired,
-        disableAmPm: propTypes.bool,
+  static displayName = 'TimePicker';
+  static defaultProps = defaultProps;
+
+  static propTypes = {
+    ...omit(CoreTimePicker.propTypes, 'useAmPm'),
+    size: propTypes.oneOf(Object.keys(Size)),
+    value: propTypes.string,
+    disableAmPm: propTypes.bool,
+  };
+
+  render() {
+    const {value, disableAmPm, size, error, width} = this.props;
+    const coreTimePickerProps = {
+      ...omit(this.props, 'size', 'value', 'disableAmPm'),
+      value,
+      useAmPm: disableAmPm ? AmPmOptions.None : AmPmOptions.Uppercase,
     };
-    static displayName = 'TimePicker';
 
-    render() {
-        const {value, disableAmPm, size, error, width} = this.props;
-        const coreTimePickerProps = {
-            ...omit(this.props, 'size', 'value', 'disableAmPm'),
-            value: format(value, 'HH:mm'),
-            useAmPm: disableAmPm ? AmPmOptions.None : AmPmOptions.Uppercase,
-        };
-
-        return (
-            <CoreTimePicker
-                {...coreTimePickerProps}
-                {...style('root', { size, error, inputWidth: getInputWidthState(width, size, disableAmPm) }, coreTimePickerProps)}
-            />
-        );
-    }
+    return (
+      <CoreTimePicker
+      {...coreTimePickerProps}
+      {...style('root', { size, error, inputWidth: getInputWidthState(width, size, disableAmPm) }, coreTimePickerProps)}
+      />
+    );
+  }
 }
