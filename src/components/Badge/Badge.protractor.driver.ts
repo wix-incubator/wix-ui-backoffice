8f5810f015938434025f0a091a45efcc1e249e24
Yarden Sod-Moriah <yardnsm@gmail.com>
2018-10-07 12:56:56 +0300

    <Badge/> - add e2e tests

--- src/components/Badge/Badge.protractor.driver.ts
+++ src/components/Badge/Badge.protractor.driver.ts
@@ -1,12 +1,16 @@
+import {isFocused} from 'wix-ui-test-utils/protractor';
 import {BaseDriver, DriverFactory} from 'wix-ui-core/dist/src/common/BaseDriver.protractor';
 
 export interface BadgeDriver extends BaseDriver {
   text: () => Promise<string>;
+  hasFocusState: () => Promise<boolean>;
 }
 
 export const badgeDriverFactory: DriverFactory<BadgeDriver> = component => ({
   /** returns the component element */
   element: () => component,
   /** returns the component text */
-  text: async () => component.getText()
+  text: async () => component.getText(),
+  /** return `true` if the element is focused */
+  hasFocusState: async () => isFocused(component)
 });
