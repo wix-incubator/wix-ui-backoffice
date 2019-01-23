448d83d00b2d5fd28926e84a24bdc0a9b8761b6c
Erez Makavy <erezm@wix.com>
2019-01-23 11:21:32 +0200

    `<FloatingHelper/>` - (fix) Add support for `dataHook` prop (#256)
    * Wrap FloatingHelper with createHOC
    
    * Remove unused imports

--- src/components/FloatingHelper/index.ts
+++ src/components/FloatingHelper/index.ts
@@ -1,2 +1,6 @@
+import {createHOC} from 'wix-ui-core/dist/src/createHOC';
+import {FloatingHelper as FLoatingHelperRaw} from './FloatingHelper';
 export * from './FloatingHelper';
+export const FloatingHelper = createHOC(FLoatingHelperRaw);
+
 export * from './constants';
