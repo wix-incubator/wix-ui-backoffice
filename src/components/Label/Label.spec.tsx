afe923419b5b2c42885e3f45a171010d39d7e351
Erez Makavy <erezm@wix.com>
2019-02-24 13:02:16 +0200

    `<Label/>` - Add Unidriver (#261)

--- src/components/Label/Label.spec.tsx
+++ src/components/Label/Label.spec.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import {labelDriverFactory} from './Label.driver';
+import {labelUniDriverFactory} from './Label.uni.driver';
 import {Label} from './';
 import {Size} from './constants';
 import {createDriverFactory} from 'wix-ui-test-utils/driver-factory';
+import {createUniDriverFactory} from 'wix-ui-test-utils/uni-driver-factory';
 import {isEnzymeTestkitExists} from 'wix-ui-test-utils/enzyme';
 import {isTestkitExists} from 'wix-ui-test-utils/vanilla';
 import {mount} from 'enzyme';
@@ -10,26 +12,35 @@ import {labelTestkitFactory} from '../../testkit';
 import {labelTestkitFactory as enzymeLabelTestkitFactory} from '../../testkit/enzyme';
 
 describe('Label', () => {
-  const createDriver = createDriverFactory(labelDriverFactory);
-
-  describe('size prop', () => {
-    it('should be medium by default', () => {
-      const wrapper = createDriver(<Label>Hello</Label>);
-      expect(wrapper.getSize()).toBe('medium');
-    });
-
-    it('should be small', () => {
-      const wrapper = createDriver(<Label size="small">Hello</Label>);
-      expect(wrapper.getSize()).toBe('small');
-    });
+  
+  describe('[sync]', () => {
+    runTests(createDriverFactory(labelDriverFactory));
   });
 
-  describe('children prop', () => {
-    it('renders', () => {
-      const wrapper = createDriver(<Label>Hello</Label>);
-      expect(wrapper.getLabelText()).toBe('Hello');
-    });
+  describe('[async]', () => {
+    runTests(createUniDriverFactory(labelUniDriverFactory));
   });
+
+  function runTests(createDriver) {
+    describe('size prop',  () => {
+      it('should be medium by default', async() => {
+        const wrapper = createDriver(<Label>Hello</Label>);
+        expect(await wrapper.getSize()).toBe('medium');
+      });
+
+      it('should be small', async () => {
+        const wrapper = createDriver(<Label size="small">Hello</Label>);
+        expect(await wrapper.getSize()).toBe('small');
+      });
+    });
+
+    describe('children prop', () => {
+      it('renders', async () => {
+        const wrapper = createDriver(<Label>Hello</Label>);
+        expect(await wrapper.getLabelText()).toBe('Hello');
+      });
+    });
+  }
 
   describe('testkit', () => {
     it('should exist', () => {
---
+++ src/components/Label/Label.uni.driver.ts
@@ -0,0 +1,23 @@
+import {UniDriver} from 'unidriver';
+
+import {StylableUnidriverUtil} from 'wix-ui-test-utils/unidriver';
+import {labelUniDriverFactory as coreLabelUniDriverFactory,LabelDriver as CoreLabelDriver} from 'wix-ui-core/dist/src/components/deprecated/label/Label.uni.driver';
+
+import {Size} from './constants';
+import styles from './Label.st.css';
+
+export interface LabelDriver extends CoreLabelDriver {
+  /** 
+   * Get size
+   */
+  getSize: () => Promise<Size>;
+}
+
+export const labelUniDriverFactory = (base: UniDriver): LabelDriver => {
+  const stylableUnidriverUtil = new StylableUnidriverUtil(styles);
+
+  return {
+    ...coreLabelUniDriverFactory(base),
+    getSize: () => stylableUnidriverUtil.getStyleState(base, 'size') as Promise<Size>
+  }
+};
