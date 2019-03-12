e0fc723b4ac478391f9b5fa4881237c381a4fa95
Nitay Neeman <nitayneeman@users.noreply.github.com>
2019-03-12 17:42:57 +0200

    <LinearProgressBar/> - fix error in driver when component is not rendered (#262)
    * <LinearProgressBar/> - wrapping the tooltip driver by a function
    
    * <LinearProgressBar/> - added test

--- src/components/LinearProgressBar/LinearProgressBar.driver.ts
+++ src/components/LinearProgressBar/LinearProgressBar.driver.ts
@@ -1,9 +1,10 @@
-import {ComponentFactory} from 'wix-ui-test-utils/driver-factory';
+import { ComponentFactory } from 'wix-ui-test-utils/driver-factory';
 import {
-  linearProgressBarDriverFactory as coreLinearProgressBarDriverFactory,
-  LinearProgressBarDriver as CoreLinearProgressBarDriver } from 'wix-ui-core/drivers/vanilla';
-import {BaseDriver, DriverFactory} from 'wix-ui-test-utils/driver-factory';
-import {tooltipDriverFactory} from '../Tooltip/Tooltip.driver'
+    linearProgressBarDriverFactory as coreLinearProgressBarDriverFactory,
+    LinearProgressBarDriver as CoreLinearProgressBarDriver
+} from 'wix-ui-core/drivers/vanilla';
+import { BaseDriver, DriverFactory } from 'wix-ui-test-utils/driver-factory';
+import { tooltipDriverFactory } from '../Tooltip/Tooltip.driver'
 
 export interface LinearProgressBarDriver extends CoreLinearProgressBarDriver {
     /* Returns true if the tooltip is shown */
@@ -18,15 +19,15 @@ export interface LinearProgressBarDriver extends CoreLinearProgressBarDriver {
 }
 
 export const linearProgressBarDriverFactory: DriverFactory<LinearProgressBarDriver> = ({ element, eventTrigger, wrapper }: ComponentFactory): LinearProgressBarDriver => {
-    const tooltipDriver = tooltipDriverFactory({element: element.querySelector(`[data-hook='linear-progressbar-tooltip']`), wrapper, eventTrigger});
-    const coreProgressBarDriver = coreLinearProgressBarDriverFactory({element, wrapper, eventTrigger});
+    const createTooltipDriver = () => tooltipDriverFactory({ element: element.querySelector(`[data-hook='linear-progressbar-tooltip']`), wrapper, eventTrigger });
+    const coreProgressBarDriver = coreLinearProgressBarDriverFactory({ element, wrapper, eventTrigger });
     const errorIcon = () => element.querySelector(`[data-hook='error-icon']`);
     const successIcon = () => element.querySelector(`[data-hook='success-icon']`);
 
     return {
         ...coreProgressBarDriver,
-        isTooltipShown: () => tooltipDriver.isContentElementExists(),
-        getTooltip: () => tooltipDriver, 
+        isTooltipShown: () => createTooltipDriver().isContentElementExists(),
+        getTooltip: () => createTooltipDriver(),
         isErrorIconShown: () => !!errorIcon(),
         isSuccessIconShown: () => !!successIcon()
     };
