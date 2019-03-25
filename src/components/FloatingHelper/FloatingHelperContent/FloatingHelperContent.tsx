db3691d60da6f69b2e13af61d8cff02699a99248
argshook <argshook@gmail.com>
2019-03-25 11:32:35 +0200

    fix(FloatingHelperContent): refactor to use non deprecated ButtonNext

--- src/components/FloatingHelper/FloatingHelperContent/FloatingHelperContent.tsx
+++ src/components/FloatingHelper/FloatingHelperContent/FloatingHelperContent.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
+import * as classnames from 'classnames';
+
 import style from './FloatingHelperContent.st.css';
 import { Text } from '../../../components/Text';
 import { DataHooks } from './DataHooks';
-import { Button, ButtonProps, ButtonSkin, ButtonPriority, ButtonSize } from '../../Button';
+import {
+  Button,
+  ButtonProps,
+  ButtonSkin,
+  ButtonPriority,
+  ButtonSize
+} from '../../Button';
+
+import { ButtonNext } from 'wix-ui-core/button-next';
+import { button } from 'wix-ui-core/themes/backoffice';
+
 import { ActionButtonTheme } from './constants';
-import {Appearance} from '../constants';
+import { Appearance } from '../constants';
 import { enumValues } from '../../../utils';
 
 export interface FloatingHelperContentProps {
@@ -17,56 +29,89 @@ export interface FloatingHelperContentProps {
   /** Sets the theme of the action button */
   actionTheme?: ActionButtonTheme;
   /** When both onActionClick & actionText are provided, will make an action button appear and invoke onActionClick() upon click */
-  onActionClick?:() => void,
+  onActionClick?: () => void;
   /** Adds an image */
   image?: React.ReactNode;
   /* Appearance : `dark` or `light`. */
   appearance?: Appearance;
 }
 
-const themeToButtonProps: { [key in ActionButtonTheme]: Pick<ButtonProps, 'skin' | 'priority'> } = {
-  [ActionButtonTheme.white]: { skin: ButtonSkin.white, priority: ButtonPriority.secondary },
-  [ActionButtonTheme.standard]: { skin: ButtonSkin.standard, priority: ButtonPriority.secondary },
-  [ActionButtonTheme.premium]: { skin: ButtonSkin.premium, priority: ButtonPriority.primary }
-}
+const themeToButtonProps: {
+  [key in ActionButtonTheme]: Pick<ButtonProps, 'skin' | 'priority'>
+} = {
+  [ActionButtonTheme.white]: {
+    skin: ButtonSkin.white,
+    priority: ButtonPriority.secondary
+  },
+  [ActionButtonTheme.standard]: {
+    skin: ButtonSkin.standard,
+    priority: ButtonPriority.secondary
+  },
+  [ActionButtonTheme.premium]: {
+    skin: ButtonSkin.premium,
+    priority: ButtonPriority.primary
+  }
+};
 
 export const FloatingHelperContent: React.SFC<FloatingHelperContentProps> = (
   props: FloatingHelperContentProps
 ) => {
-  const { title, body, actionText, onActionClick, actionTheme, image, appearance } = props;
+  const {
+    title,
+    body,
+    actionText,
+    onActionClick,
+    actionTheme,
+    image,
+    appearance
+  } = props;
 
   return (
     <div {...style('root', { hasBody: !!props.body }, props)}>
       <div>
         {title && (
           <div className={style.title}>
-            <Text data-hook={DataHooks.title} bold light={appearance === Appearance.dark}>
+            <Text
+              data-hook={DataHooks.title}
+              bold
+              light={appearance === Appearance.dark}
+            >
               {title}
             </Text>
           </div>
         )}
         {body && (
           <div className={style.body}>
-            <Text data-hook={DataHooks.body} light={appearance === Appearance.dark}>
+            <Text
+              data-hook={DataHooks.body}
+              light={appearance === Appearance.dark}
+            >
               {body}
             </Text>
           </div>
         )}
-        {actionText && onActionClick &&
-          actionText.length > 0 && (
-            <Button
-              className={style.action}
-              data-hook={DataHooks.actionButton}
-              skin={themeToButtonProps[actionTheme].skin}
-              priority={themeToButtonProps[actionTheme].priority}
-              size={ButtonSize.small}
-              onClick={onActionClick}
-            >
-              {actionText}
-            </Button>
-          )}
+        {actionText && onActionClick && actionText.length > 0 && (
+          <ButtonNext
+            className={classnames(
+              style.action,
+              button(
+                themeToButtonProps[actionTheme].skin,
+                themeToButtonProps[actionTheme].priority,
+                ButtonSize.small
+              )
+            )}
+            data-hook={DataHooks.actionButton}
+            onClick={onActionClick}
+          >
+            {actionText}
+          </ButtonNext>
+        )}
       </div>
-      {image && <div data-hook={DataHooks.image} className={style.image}>{image}</div>}
+      {image && (
+        <div data-hook={DataHooks.image} className={style.image}>
+          {image}
+        </div>
+      )}
     </div>
   );
 };
