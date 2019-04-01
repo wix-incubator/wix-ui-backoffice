99b9456c5007716934db147938c0c491b9d353e0
Erez Makavy <erezm@wix.com>
2019-04-01 09:13:21 +0300

    Upgrade unidriver

--- src/components/Label/Label.uni.driver.ts
+++ src/components/Label/Label.uni.driver.ts
@@ -1,4 +1,4 @@
-import {UniDriver} from 'unidriver';
+import {UniDriver} from '@unidriver/core';
 
 import {StylableUnidriverUtil} from 'wix-ui-test-utils/unidriver';
 import {labelUniDriverFactory as coreLabelUniDriverFactory,LabelDriver as CoreLabelDriver} from 'wix-ui-core/dist/src/components/deprecated/label/Label.uni.driver';
