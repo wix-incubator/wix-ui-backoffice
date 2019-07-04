c972261a5a362d2abbac3c790774dfe038238066
Juste Noreikaite <justen@wix.com>
2019-07-04 15:18:50 +0300

    <fullTextView /> - add unidriver

---
+++ src/components/FullTextView/FullTextView.uni.driver.tsx
@@ -0,0 +1,18 @@
+import {
+  BaseUniDriver,
+  baseUniDriverFactory,
+} from 'wix-ui-test-utils/base-driver';
+import {UniDriver} from 'wix-ui-test-utils/unidriver';
+
+export interface FullTextViewUniDriver extends BaseUniDriver {
+  getText: () => Promise<String>;
+  getTagName: () => String;
+}
+
+export const fullTextViewUniDriverFactory = (base: UniDriver): FullTextViewUniDriver => {
+  return {
+    ...baseUniDriverFactory(base),
+    getText: () => base.text() as Promise<String>,
+    getTagName: async () => (await base._prop('tagName')).toLowerCase() as String,
+  }
+};
